d5637a81df5e6df3e09ce50d63c3ba80e70f8f54
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-08-17 20:01:53 +0200

    Updated catch include

--- tests/test_version.cpp
+++ tests/test_version.cpp
@@ -26,7 +26,7 @@
  */
 
 
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 #include <xmipp4/version.hpp>
 
--- tests/utils/test_byte_order.cpp
+++ tests/utils/test_byte_order.cpp
@@ -25,7 +25,7 @@
  * @date 2023-08-12
  */
 
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 #include <xmipp4/utils/byte_order.hpp>
 

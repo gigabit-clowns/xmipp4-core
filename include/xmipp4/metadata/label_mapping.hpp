d6e8ee6dc235566833c0bd524ece35105dfe5ce3
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-08-25 17:08:32 +0200

    Added label mapping

--- include/xmipp4/metadata/label_mapping.hpp
+++ include/xmipp4/metadata/label_mapping.hpp
@@ -20,6 +20,7 @@
  *  e-mail address 'xmipp@cnb.csic.es'
  ***************************************************************************/
 
+#include <string>
 #include <vector>
 #include <unordered_map>
 
@@ -32,6 +33,7 @@ class label_mapping
 {
 public:
     label_mapping() = default;
+    label_mapping(std::vector<std::string> labels);
     label_mapping(const label_mapping& other) = default;
     label_mapping(label_mapping&& other) = default;
     ~label_mapping() = default;
@@ -39,14 +41,23 @@ public:
     label_mapping& operator=(const label_mapping& other) = default;
     label_mapping& operator=(label_mapping&& other) = default;
 
+    void set_labels(std::vector<std::string> labels);
+    const std::vector<std::string>& get_labels() const noexcept;
+
+    void reserve(std::size_t capacity);
+    bool add_label(std::string label);
+
+    std::size_t get_index(std::string_view label) const;
+
 private:
+    using index_map = std::unordered_map<std::string_view, std::size_t>;
+
     std::vector<std::string> m_labels;
-    std::unordered_map<std::string_view, std::size_t> m_label_to_index;
+    index_map m_label_to_index;
+
+    static index_map compute_index_mapping(std::vector<std::string>& labels);
 
 };
 
 } // namespace metadata
 } // namespace xmipp4
-
-#include "column.inl"
-

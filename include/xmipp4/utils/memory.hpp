4574f3f4bb42720e00a97135e954669a6ed80267
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-08-31 15:53:22 +0200

    Added documentation

--- include/xmipp4/utils/memory.hpp
+++ include/xmipp4/utils/memory.hpp
@@ -20,6 +20,14 @@
  *  e-mail address 'xmipp@cnb.csic.es'
  ***************************************************************************/
 
+/**
+ * @file memory.hpp
+ * @author Oier Lauzirika Zarrabeitia (oierlauzi@bizkaia.eu)
+ * @brief Utility functions related to memory address manipulations
+ * @date 2023-08-31
+ * 
+ */
+
 #include "../platform/attributes.hpp"
 #include "../platform/constexpr.hpp"
 
@@ -30,19 +38,102 @@ namespace xmipp4
 namespace utils
 {
 
+/**
+ * @brief Get the alignment of a pointer
+ * 
+ * Finds the largest power of two that is a divisor of the address
+ * 
+ * @tparam T Pointed value type
+ * @param address The address of which the alignment is determined
+ * @return std::size_t The alignment (a power of two)
+ */
 template <typename T>
-XMIPP4_NODISCARD std::size_t get_alignment(T* address) noexcept;
-XMIPP4_NODISCARD XMIPP4_CONSTEXPR std::size_t get_alignment(std::uintptr_t address) noexcept;
+XMIPP4_NODISCARD 
+std::size_t get_alignment(T* address) noexcept;
+
+/**
+ * @brief Get the alignment of an address
+ * 
+ * Finds the largest power of two that is a divisor of the address
+ * 
+ * @param address The address of which the alignment is determined
+ * @return std::size_t The alignment (a power of two)
+ */
+XMIPP4_NODISCARD XMIPP4_CONSTEXPR 
+std::size_t get_alignment(std::uintptr_t address) noexcept;
+
+/**
+ * @brief Align a pointer to a lower or equal address that satisfies the
+ * provided alignment
+ * 
+ * @tparam T Pointed value type
+ * @param address The address to be aligned
+ * @param alignment The minimum alignment
+ * @return T* Aligned pointer
+ */
+template <typename T>
+XMIPP4_NODISCARD 
+T* align_floor(T* address, std::size_t alignment) noexcept;
+
+/**
+ * @brief Align an address to a lower or equal address that satisfies the
+ * provided alignment
+ * 
+ * @param address The address to be aligned
+ * @param alignment The minimum alignment
+ * @return std::uintptr_t Aligned address
+ */
+XMIPP4_NODISCARD XMIPP4_CONSTEXPR 
+std::uintptr_t align_floor(std::uintptr_t address, std::size_t alignment) noexcept;
+
+/**
+ * @brief Align an address to a lower or equal address that satisfies the
+ * provided alignment
+ * 
+ * @param address The address to be aligned
+ * @param alignment The minimum alignment
+ * @return std::uintptr_t Aligned address
+ * @note The result is written in place and a reference is returned
+ */
+XMIPP4_CONSTEXPR 
+std::uintptr_t& align_floor_inplace(std::uintptr_t& address, std::size_t alignment) noexcept;
+
+/**
+ * @brief Align a pointer to a higher or equal address that satisfies the
+ * provided alignment
+ * 
+ * @tparam T Pointed value type
+ * @param address The address to be aligned
+ * @param alignment The minimum alignment
+ * @return T* Aligned pointer
+ */
 
 template <typename T>
-XMIPP4_NODISCARD T* align_floor(T* address, std::size_t alignment) noexcept;
-XMIPP4_NODISCARD XMIPP4_CONSTEXPR std::uintptr_t align_floor(std::uintptr_t address, std::size_t alignment) noexcept;
-XMIPP4_CONSTEXPR std::uintptr_t& align_floor_inplace(std::uintptr_t& address, std::size_t alignment) noexcept;
+XMIPP4_NODISCARD 
+T* align_ceil(T* address, std::size_t alignment) noexcept;
 
-template <typename T>
-XMIPP4_NODISCARD T* align_ceil(T* address, std::size_t alignment) noexcept;
-XMIPP4_NODISCARD XMIPP4_CONSTEXPR std::uintptr_t align_ceil(std::uintptr_t address, std::size_t alignment) noexcept;
-XMIPP4_CONSTEXPR std::uintptr_t& align_ceil_inplace(std::uintptr_t& address, std::size_t alignment) noexcept;
+/**
+ * @brief Align an address to a higher or equal address that satisfies the
+ * provided alignment
+ * 
+ * @param address The address to be aligned
+ * @param alignment The minimum alignment
+ * @return std::uintptr_t Aligned address
+ */
+XMIPP4_NODISCARD XMIPP4_CONSTEXPR 
+std::uintptr_t align_ceil(std::uintptr_t address, std::size_t alignment) noexcept;
+
+/**
+ * @brief Align an address to a higher or equal address that satisfies the
+ * provided alignment
+ * 
+ * @param address The address to be aligned
+ * @param alignment The minimum alignment
+ * @return std::uintptr_t Aligned address
+ * @note The result is written in place and a reference is returned
+ */
+XMIPP4_CONSTEXPR 
+std::uintptr_t& align_ceil_inplace(std::uintptr_t& address, std::size_t alignment) noexcept;
 
 } // namespace utils
 } // namespace xmipp4

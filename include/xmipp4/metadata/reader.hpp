316426230db4e7ebd910b61bac2325857c6144bc
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2023-09-20 19:59:22 +0200

    Added I/O interfaces

---
+++ include/xmipp4/metadata/reader.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+/***************************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+
+#include <cstddef>
+
+namespace xmipp4
+{
+namespace metadata
+{
+
+class table;
+
+class reader
+{
+public:
+    reader() = default;
+    reader(const reader& other) = delete;
+    reader(reader&& other) = default;
+    virtual ~reader() = default;
+
+    reader& operator=(const reader& other) = delete;
+    reader& operator=(reader&& other) = default;
+
+    virtual std::size_t read(table& table) = 0;
+    virtual std::size_t read(table& table, std::size_t count) = 0;
+
+    virtual std::size_t tell() const noexcept = 0;
+
+};
+
+} // namespace metadata
+} // namespace xmipp4

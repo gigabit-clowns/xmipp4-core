47ef63bfc8a3a26990dda99d6e1a3331425640c1
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-09-26 17:38:52 +0200

    Re added failing requirements

--- tests/concurrency/test_semaphore.cpp
+++ tests/concurrency/test_semaphore.cpp
@@ -94,7 +94,7 @@ TEST_CASE("try acquire semaphore with absolute timeout", "[counting_semaphore]")
     const auto now = std::chrono::steady_clock::now();
     const auto timeout = now + std::chrono::milliseconds(500);
     REQUIRE( !sem.try_acquire_until(timeout) ); // Should wait
-    // REQUIRE( timeout <= std::chrono::steady_clock::now() ); // Unreliable
+    REQUIRE( timeout <= std::chrono::steady_clock::now() );
 
     const auto past_timeout = now - std::chrono::milliseconds(500);
     REQUIRE( !sem.try_acquire_until(past_timeout) ); // Should instantly return
@@ -124,7 +124,7 @@ TEST_CASE("try acquire semaphore with relative timeout", "[counting_semaphore]")
     const auto now = std::chrono::steady_clock::now();
     const auto timeout = std::chrono::milliseconds(500);
     REQUIRE( !sem.try_acquire_for(timeout) ); // Should wait
-    // REQUIRE( (now + timeout) <= std::chrono::steady_clock::now() ); // Unreliable
+    REQUIRE( (now + timeout) <= std::chrono::steady_clock::now() );
 
     const auto past_timeout = -std::chrono::milliseconds(500);
     REQUIRE( !sem.try_acquire_for(past_timeout) ); // Should instantly return

c72edb77ec2735b183760c4c8680dcd83bfc0ee7
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-10-20 23:45:59 +0200

    Removed windows implementation

--- include/xmipp4/core/concurrency/semaphore.hpp
+++ include/xmipp4/core/concurrency/semaphore.hpp
@@ -31,13 +31,8 @@
 #else 
     // Fall back to one of the manual implementations or wrappers
     #include "detail/posix_semaphore.hpp"
-    #include "detail/windows_semaphore.hpp"
     #include "detail/mutex_semaphore.hpp"
-    #if defined(XMIPP4_WINDOWS_SEMAPHORE_IMPLEMENTATION)
-        #define XMIPP4_SEMAPHORE_IMPLEMENTATION(n) detail::windows_semaphore<n>
-        #define XMIPP4_SEMAPHORE_IMPLEMENTATION_MAX_VALUE \
-            XMIPP4_WINDOWS_SEMAPHORE_IMPLEMENTATION_MAX_VALUE
-    #elif defined(XMIPP4_POSIX_SEMAPHORE_IMPLEMENTATION)
+    #if defined(XMIPP4_POSIX_SEMAPHORE_IMPLEMENTATION)
         #define XMIPP4_SEMAPHORE_IMPLEMENTATION(n) detail::posix_semaphore
         #define XMIPP4_SEMAPHORE_IMPLEMENTATION_MAX_VALUE \
             XMIPP4_POSIX_SEMAPHORE_IMPLEMENTATION_MAX_VALUE

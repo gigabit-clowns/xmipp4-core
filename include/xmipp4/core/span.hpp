853e94c528c50bd815b9ad1924e255c550a98a6d
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-10-25 13:23:51 +0200

    Added doc to span

--- include/xmipp4/core/span.hpp
+++ include/xmipp4/core/span.hpp
@@ -36,11 +36,21 @@
 namespace xmipp4 
 {
 
+/**
+ * @brief Constant representing dynamic size of an array.
+ * 
+ */
 XMIPP4_CONSTEXPR auto dynamic_extent = std::numeric_limits<std::size_t>::max(); 
 
 namespace detail
 {
 
+/**
+ * @brief Storage for the span object.
+ * 
+ * @tparam T Element type
+ * @tparam S Static size
+ */
 template <typename T, std::size_t S>
 struct span_storage 
 {
@@ -58,6 +68,11 @@ struct span_storage
 
 };
 
+/**
+ * @brief Storage for the span object with dynamic sizing.
+ * 
+ * @tparam T 
+ */
 template <typename T>
 struct span_storage<T, dynamic_extent> 
 {
@@ -77,6 +92,15 @@ struct span_storage<T, dynamic_extent>
 } // namespace detail
 
 
+
+/**
+ * @brief Represents a non-owning area of contiguous memory.
+ * 
+ * Provides a pre C++20 implementation of std::span.
+ * 
+ * @tparam T Type of the elements.
+ * @tparam Extent Size of the area. Use dynamic_extent dynamic assignation.
+ */
 template <typename T, std::size_t Extent = dynamic_extent>
 class span 
 {
@@ -96,8 +120,22 @@ public:
 
     static constexpr size_type extent = Extent;
 
+    /**
+     * @brief Construct from a pointer and size.
+     * 
+     * @param ptr Pointer to the data.
+     * @param count Number of elements.
+     */
     XMIPP4_CONSTEXPR span(pointer ptr, size_type count);
+
+    /**
+     * @brief Construct from a pointer pair.
+     * 
+     * @param first Pointer to the first element.
+     * @param last Pointer to the past-the-end element.
+     */
     XMIPP4_CONSTEXPR span(pointer first, pointer last);
+
     span(const span &other) = default;
     span(span &&other) = default;
     ~span() = default;
@@ -105,27 +143,89 @@ public:
     span& operator=(const span &other) = default;
     span& operator=(span &&other) = default;
 
-
+    /**
+     * @brief Get the number of referenced objects.
+     * 
+     * @return size_type Number of objects.
+     */
     XMIPP4_CONSTEXPR size_type size() const noexcept;
 
+    /**
+     * @brief Get the size in bytes of the referenced objects.
+     * 
+     * @return size_type Number of bytes.
+     */
     XMIPP4_CONSTEXPR size_type size_bytes() const noexcept;
 
+    /**
+     * @brief Check if no objects are references.
+     * 
+     * @return true If 0 objects are referenced.
+     * @return false If 1 or more objects are referenced.
+     */
     XMIPP4_NODISCARD XMIPP4_CONSTEXPR bool empty() const noexcept;
 
+    /**
+     * @brief Obtain a reference to an object by its index.
+     * 
+     * @param idx The index of the object. Must be in [0...size())
+     * @return reference The reference to the requested object.
+     * @note No boundary checks are performed. Using an index that
+     * is greater or equal to the size leads to undefined behavior.
+     */
     XMIPP4_CONSTEXPR reference operator[](size_type idx) const noexcept;
 
+    /**
+     * @brief Obtain a reference to the first object.
+     * 
+     * @return reference Reference to the first object.
+     * @note No emptiness check is performed. Calling this method
+     * when empty() == true leads to undefined behavior.
+     */
     XMIPP4_CONSTEXPR reference front() const noexcept;
 
+    /**
+     * @brief Obtain a reference to the last object.
+     * 
+     * @return reference Reference to the last object.
+     * @note No emptiness check is performed. Calling this method
+     * when empty() == true leads to undefined behavior.
+     */
     XMIPP4_CONSTEXPR reference back() const noexcept;
 
+    /**
+     * @brief Get a pointer to the referenced data.
+     * 
+     * @return pointer Pointer to the referenced data.
+     */
     XMIPP4_CONSTEXPR pointer data() const noexcept;
 
+    /**
+     * @brief Get an iterator to the first item.
+     * 
+     * @return iterator Iterator to the first item. 
+     */
     XMIPP4_CONSTEXPR iterator begin() const noexcept;
 
+    /**
+     * @brief Get an iterator to the past-the-end item.
+     * 
+     * @return iterator Iterator to the past the end item.
+     */
     XMIPP4_CONSTEXPR iterator end() const noexcept;
 
+    /**
+     * @brief Get a reverse iterator to the last item.
+     * 
+     * @return reverse_iterator Reverse iterator to the last item. 
+     */
     XMIPP4_CONSTEXPR reverse_iterator rbegin() const noexcept;
 
+    /**
+     * @brief Get a reverse iterator to the before-first item.
+     * 
+     * @return reverse_iterator Reverse iterator to the before-first item. 
+     */
     XMIPP4_CONSTEXPR reverse_iterator rend() const noexcept;
 
 private:

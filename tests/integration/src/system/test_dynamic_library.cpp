2fc060da0eed1ac8465428130874e1405d13d9cd
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-10-28 20:34:03 +0100

    Added test for query_symbol_filename

--- tests/integration/src/system/test_dynamic_library.cpp
+++ tests/integration/src/system/test_dynamic_library.cpp
@@ -38,12 +38,12 @@ using namespace xmipp4;
 
 TEST_CASE( "open xmipp4-core as dynamic library", "[dynamic_library]" ) 
 {
-    system::dynamic_library xmipp4_core(get_mock_plugin_path("dummy_plugin"));
+    system::dynamic_library dl(get_mock_plugin_path("dummy_plugin"));
 
-    REQUIRE( xmipp4_core.is_open() );
-    REQUIRE( xmipp4_core.get_symbol("lorem_ipsum") == nullptr );
+    REQUIRE( dl.is_open() );
+    REQUIRE( dl.get_symbol("lorem_ipsum") == nullptr );
 
-    const auto *symbol = xmipp4_core.get_symbol("xmipp4_get_plugin");
+    const auto *symbol = dl.get_symbol("xmipp4_get_plugin");
     REQUIRE( symbol != nullptr );
 }
 
@@ -51,4 +51,19 @@ TEST_CASE( "default construct dynamic_library", "[dynamic_library]" )
 {
     system::dynamic_library lib;
     REQUIRE( lib.is_open() == false );
+}
+
+TEST_CASE( "recover shared library path from symbol", "[dynamic_library]" ) 
+{
+    // Open a dynamic library and query a symbol.
+    const auto path = get_mock_plugin_path("dummy_plugin");
+    system::dynamic_library dl(path);
+    const auto *symbol = dl.get_symbol("xmipp4_get_plugin");
+
+    // Recover path from loaded symbol
+    const auto path2 = system::dynamic_library::query_symbol_filename(symbol);
+
+    // The last part of the path should coincide even if the returned
+    // path is absolute
+    REQUIRE( path2.ends_with(path) );
 }

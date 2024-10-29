99fa39ef1b17231d9630670ce730f2e827c879fc
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-10-29 11:08:58 +0100

    Documented device type

--- include/xmipp4/core/compute/device_type.hpp
+++ include/xmipp4/core/compute/device_type.hpp
@@ -29,14 +29,17 @@ namespace xmipp4
 namespace compute
 {
 
+/**
+ * @brief Device categories.
+ * 
+ */
 enum class device_type
 {
-    unknown = -1,
-    cpu,
-    gpu,
-    integrated_gpu,
-    tpu,
-    fpga
+    unknown = -1, ///< The device does fall into any of the following categories
+    cpu, ///< CPU of the host.
+    gpu, ///< A discrete Graphics Processing Unit (GPU)
+    integrated_gpu, ///< An integrated Graphics Processing Unit (GPU)
+    fpga, ///< A Field Programmable Grid Array (FPGA)
 };
 
 XMIPP4_CONSTEXPR const char* to_string(device_type type) noexcept;

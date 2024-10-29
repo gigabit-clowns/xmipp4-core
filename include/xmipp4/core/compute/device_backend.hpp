1e1efd713a831d9701d3c18d2f26107521f1e86b
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-10-29 10:53:22 +0100

    Documented device backend interface

--- include/xmipp4/core/compute/device_backend.hpp
+++ include/xmipp4/core/compute/device_backend.hpp
@@ -41,6 +41,10 @@ namespace compute
 
 class device;
 
+/**
+ * @brief Abstract class that represents a compute backend.
+ * 
+ */
 class device_backend
     : public backend
 {
@@ -53,10 +57,40 @@ public:
     device_backend& operator=(const device_backend &other) = default;
     device_backend& operator=(device_backend &&other) = default;
 
+    /**
+     * @brief Enumerate available in devices within this backend.
+     * 
+     * @param ids IDs of the devices. 
+     * @note The IDs are only unique within each backend.
+     * 
+     */
     virtual void enumerate_devices(std::vector<std::size_t> &ids) const = 0;
+
+    /**
+     * @brief Query the properties of a device.
+     * 
+     * @param id Identifier of the device.
+     * @param desc Output properties.
+     * @return true The device ID exists. Properties were written.
+     * @return false The device ID does not exist. Properties were not written.
+     * 
+     */
     virtual bool get_device_properties(std::size_t id, device_properties &desc) const = 0;
 
+    /**
+     * @brief Create a device handle for the given device identifier.
+     * 
+     * @param id The identifier of the device.
+     * @return std::unique_ptr<device> The device handle.
+     */
     virtual std::unique_ptr<device> create_device(std::size_t id) = 0;
+
+    /**
+     * @brief Create a device handle for the given device identifier.
+     * 
+     * @param id The identifier of the device.
+     * @return std::shared_ptr<device> The device handle.
+     */
     virtual std::shared_ptr<device> create_device_shared(std::size_t id) = 0;
 
 }; 

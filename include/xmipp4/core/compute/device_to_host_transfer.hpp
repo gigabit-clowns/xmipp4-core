c97622278489db400f016fe459e4a6be7d31a540
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-11-07 15:39:30 +0100

    Added wait

--- include/xmipp4/core/compute/device_to_host_transfer.hpp
+++ include/xmipp4/core/compute/device_to_host_transfer.hpp
@@ -128,6 +128,12 @@ public:
                     host_memory_allocator &allocator,
                     device_queue &queue ) const = 0;
 
+    /**
+     * @brief Block the current thread until the transfers have finished.
+     * 
+     */
+    virtual void wait() = 0;
+
 }; 
 
 } // namespace compute

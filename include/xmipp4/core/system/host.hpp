e763268f61ec8a3d2118d1c5cd92c8446a7940db
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-11-08 21:18:02 +0100

    Added available system memory

--- include/xmipp4/core/system/host.hpp
+++ include/xmipp4/core/system/host.hpp
@@ -49,5 +49,12 @@ std::string get_hostname();
  */
 std::size_t get_total_system_memory();
 
+/**
+ * @brief Get the memory available in the system.
+ * 
+ * @return std::size_t Total memory in bytes.
+ */
+std::size_t get_available_system_memory();
+
 } // namespace system
 } // namespace xmipp4

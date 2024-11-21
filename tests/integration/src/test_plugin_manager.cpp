bc7617b7cf9897453cde182274f8a3e7ef497aaf
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-11-21 13:13:57 +0100

    Returning system error

--- tests/integration/src/test_plugin_manager.cpp
+++ tests/integration/src/test_plugin_manager.cpp
@@ -26,13 +26,15 @@
  * 
  */
 
+#include <xmipp4/core/plugin_manager.hpp>
+
+#include <xmipp4/core/plugin.hpp>
+#include <xmipp4/core/exceptions/plugin_load_error.hpp>
+
 #include "assets.hpp"
 
 #include <catch2/catch_test_macros.hpp>
-
-#include <xmipp4/core/plugin.hpp>
-#include <xmipp4/core/plugin_manager.hpp>
-#include <xmipp4/core/platform/operating_system.h>
+#include <catch2/matchers/catch_matchers.hpp>
 
 using namespace xmipp4;
 
@@ -48,25 +50,49 @@ TEST_CASE( "load good plugin in the plugin manager", "[plugin_manager]" )
     REQUIRE( &(manager.get_plugin(0)) == plugin );
 }
 
-TEST_CASE( "load invalid plugin in the plugin manager", "[plugin_manager]" ) 
+TEST_CASE( "load invalid plugin in the plugin manager should throw", "[plugin_manager]" ) 
 {
     plugin_manager manager;
-    REQUIRE_THROWS( manager.load_plugin("path_to_nowhere") );
+
+    REQUIRE_THROWS_AS(
+        manager.load_plugin("path_to_nowhere"), 
+        std::system_error
+    );
     REQUIRE( manager.get_plugin_count() == 0 );
-    REQUIRE_THROWS( manager.load_plugin(get_text_file_path()) );
+
+    REQUIRE_THROWS_AS( 
+        manager.load_plugin(get_text_file_path()), 
+        std::system_error
+    );
     REQUIRE( manager.get_plugin_count() == 0 );
-    REQUIRE_THROWS( manager.load_plugin(get_mock_plugin_path("faulty_plugin1")) );
+
+    REQUIRE_THROWS_AS( 
+        manager.load_plugin(get_mock_plugin_path("faulty_plugin1")), 
+        plugin_load_error 
+    );
+    REQUIRE_THROWS_WITH( 
+        manager.load_plugin(get_mock_plugin_path("faulty_plugin1")),
+        "xmipp4_get_plugin returned NULL."
+    );
     REQUIRE( manager.get_plugin_count() == 0 );
-    REQUIRE_THROWS( manager.load_plugin(get_mock_plugin_path("faulty_plugin2")) );
+
+    REQUIRE_THROWS_AS( 
+        manager.load_plugin(get_mock_plugin_path("faulty_plugin2")), 
+        plugin_load_error
+    );
+    REQUIRE_THROWS_WITH( 
+        manager.load_plugin(get_mock_plugin_path("faulty_plugin2")), 
+        "xmipp4_get_plugin symbol could not be found in shared object."
+    );
     REQUIRE( manager.get_plugin_count() == 0 );
 }
 
-TEST_CASE( "query out out range plugin from plugin manager", "[plugin_manager]" ) 
+TEST_CASE( "querying out out range plugin from plugin manager should throw", "[plugin_manager]" ) 
 {
     plugin_manager manager;
     manager.load_plugin(get_mock_plugin_path("dummy_plugin"));
-    REQUIRE_THROWS( manager.get_plugin(1) );
-    REQUIRE_THROWS( manager.get_plugin(10) );
+    REQUIRE_THROWS_AS( manager.get_plugin(1), std::out_of_range );
+    REQUIRE_THROWS_AS( manager.get_plugin(10), std::out_of_range );
 }
 
 TEST_CASE( "discover plugins", "[plugin_manager]" ) 

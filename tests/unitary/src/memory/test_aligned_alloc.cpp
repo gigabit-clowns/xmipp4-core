9c2114963ffd697dc7b71a83627e98f03c8640fd
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-12-02 16:22:06 +0100

    Fix codesmell

--- tests/unitary/src/memory/test_aligned_alloc.cpp
+++ tests/unitary/src/memory/test_aligned_alloc.cpp
@@ -43,7 +43,7 @@ TEST_CASE( "aligned_alloc should produce aligned allocations", "[aligned_alloc]"
 
 	for (std::size_t i = 0; i < repetitions; ++i)
 	{
-		char* data = static_cast<char*>(memory::aligned_alloc(size, alignment));
+		auto *data = static_cast<char*>(memory::aligned_alloc(size, alignment));
 		REQUIRE( data != nullptr ); // Assuming no OOM situation. 
 		REQUIRE( memory::get_alignment(data) >= alignment );
 

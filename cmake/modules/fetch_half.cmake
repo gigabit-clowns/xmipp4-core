554793a0c4dbc8a3cc4f37561397a3b2c8826fbd
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-03-23 17:20:19 +0100

    Using tabs

--- cmake/modules/fetch_half.cmake
+++ cmake/modules/fetch_half.cmake
@@ -25,46 +25,46 @@ cmake_minimum_required(VERSION 3.12)
 include(FetchContent)
 
 function(fetch_half)
-    set(options)
-    set(oneValueArgs VERSION)
-    set(multiValueArgs)
-    cmake_parse_arguments(PARSE_ARGV 0 arg
-        "${options}" "${oneValueArgs}" "${multiValueArgs}"
-    )
+	set(options)
+	set(oneValueArgs VERSION)
+	set(multiValueArgs)
+	cmake_parse_arguments(PARSE_ARGV 0 arg
+		"${options}" "${oneValueArgs}" "${multiValueArgs}"
+	)
 
 	cmake_policy(SET CMP0135 NEW) # To avoid warnings
-    set(NO_OP_COMMAND "${CMAKE_COMMAND} -E true")
-    FetchContent_Declare(
-        half
-        URL https://kumisystems.dl.sourceforge.net/project/half/half/${arg_VERSION}/half-${arg_VERSION}.zip
-        CONFIGURE_COMMAND ${NO_OP_COMMAND}
-        BUILD_COMMAND ${NO_OP_COMMAND}
-        INSTALL_COMMAND ${NO_OP_COMMAND}
-    )
-    FetchContent_MakeAvailable(half)
+	set(NO_OP_COMMAND "${CMAKE_COMMAND} -E true")
+	FetchContent_Declare(
+		half
+		URL https://kumisystems.dl.sourceforge.net/project/half/half/${arg_VERSION}/half-${arg_VERSION}.zip
+		CONFIGURE_COMMAND ${NO_OP_COMMAND}
+		BUILD_COMMAND ${NO_OP_COMMAND}
+		INSTALL_COMMAND ${NO_OP_COMMAND}
+	)
+	FetchContent_MakeAvailable(half)
 
 	set(half_INCLUDE_DIR ${half_SOURCE_DIR}/include)
 
 	# Define the target
 	add_library(half INTERFACE)
 	target_include_directories(
-        half 
-        INTERFACE
-            $<BUILD_INTERFACE:${half_INCLUDE_DIR}>
-            $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
-    )
+		half 
+		INTERFACE
+			$<BUILD_INTERFACE:${half_INCLUDE_DIR}>
+			$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
+	)
 
-    # Define install command
-    install(FILES ${half_INCLUDE_DIR}/half.hpp DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-    install(
-        TARGETS half
-        EXPORT half-targets
-        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}/half
-    )
-    install(
-        EXPORT half-targets
-        FILE half-config.cmake
-        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/half
-    )
+	# Define install command
+	install(FILES ${half_INCLUDE_DIR}/half.hpp DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+	install(
+		TARGETS half
+		EXPORT half-targets
+		LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}/half
+	)
+	install(
+		EXPORT half-targets
+		FILE half-config.cmake
+		DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/half
+	)
 
 endfunction()

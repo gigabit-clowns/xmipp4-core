ba0d33bae84e90a56d8c019baa8c76dc40457fbc
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-06-27 14:53:38 +0200

    Added exception doc

--- include/xmipp4/core/multidimensional/checks.hpp
+++ include/xmipp4/core/multidimensional/checks.hpp
@@ -44,6 +44,7 @@ namespace multidimensional
  * @param first Iterator to the first element in the range.
  * @param last Iterator to the past-the-end element in the range.
  * @param count Number of axes in the layout.
+ * @throws std::invalid_argument If the permutation is not valid.
  */
 template <typename ForwardIt>
 void check_axis_permutation(ForwardIt first, ForwardIt last, std::size_t count);

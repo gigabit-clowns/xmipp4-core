533b619e19a2261c3b4b4eee93820824cb456f7a
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-07-03 17:23:10 +0200

    Added include

--- tests/unitary/src/multidimensional/test_broadcast.cpp
+++ tests/unitary/src/multidimensional/test_broadcast.cpp
@@ -28,6 +28,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers.hpp>
+#include <trompeloeil/sequence.hpp>
 
 #include <xmipp4/core/multidimensional/broadcast.hpp>
 

3bd54bfc5aa8c081e41b118adcd80dc639b4d432
Martin Salinas <ssalinasmartin@gmail.com>
2025-07-07 20:12:11 +0200

    Return output

--- conda/visual-studio-name-detector.ps1
+++ conda/visual-studio-name-detector.ps1
@@ -4,12 +4,12 @@ $props = & vswhere `
     -property catalog_productDisplayVersion,catalog_productLineVersion `
     -nologo
 
-Write-Host $props
+Write-Output $props
 exit 1
 
 $catalogProductDisplayVersion = $props[0].Trim()
 $catalogProductLineVersion   = $props[1].Trim()
 $catalogProductLineMajor = $catalogProductLineVersion.Split('.')[0]
 
-Write-Host "Display Version: $catalogProductDisplayVersion"
-Write-Host "Line Major:      $catalogProductLineMajor"
+Write-Output "Display Version: $catalogProductDisplayVersion"
+Write-Output "Line Major:      $catalogProductLineMajor"

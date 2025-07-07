b7b8c3efa775225438338015177782c61238ce5f
Martin Salinas <ssalinasmartin@gmail.com>
2025-07-07 19:20:05 +0200

    Attempt to extract variables

--- conda/visual-studio-name-detector.ps1
+++ conda/visual-studio-name-detector.ps1
@@ -1,14 +1,8 @@
 # PowerShell script to detect the latest Visual Studio and return a string like:
 # "Visual Studio <display version major> <year>"
 Invoke-Expression "vswhere -latest -products * -format json | ConvertFrom-Json | Select-Object -First 1"
-exit 1
-$vswhere = "$Env:ProgramFiles(x86)\Microsoft Visual Studio\Installer\vswhere.exe"
-if (!(Test-Path $vswhere)) {
-    Write-Error "vswhere.exe not found."
-    exit 1
-}
 
-$instance = & vswhere -latest -products * -format json | ConvertFrom-Json | Select-Object -First 1
+$instance = & "vswhere -latest -products * -format json | ConvertFrom-Json | Select-Object -First 1"
 if (-not $instance) {
     Write-Error "No Visual Studio installation found."
     exit 1

f54a086f4c4dfdd2e41f70df894cad2612cdf440
Martin Salinas <ssalinasmartin@gmail.com>
2025-07-07 20:39:40 +0200

    Final script

--- conda/visual-studio-name-detector.ps1
+++ conda/visual-studio-name-detector.ps1
@@ -1,28 +1,14 @@
-#$props = & vswhere `
-#    -latest `
-#    -products * `
-#    -property catalog_productDisplayVersion,catalog_productLineVersion `
-#    -nologo
-
 $displayVersion = & vswhere `
     -latest `
     -products * `
     -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
-    -property catalog_productDisplayVersion,catalog_productLineVersion
+    -property catalog_productDisplayVersion
 
-$lineVersion = & vswhere `
+$releaseYear = & vswhere `
     -latest `
     -products * `
     -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
     -property catalog_productLineVersion
 
-Write-Output $displayVersion
-Write-Output $lineVersion
-exit 1
-
-$catalogProductDisplayVersion = $props[0].Trim()
-$catalogProductLineVersion   = $props[1].Trim()
-$catalogProductLineMajor = $catalogProductLineVersion.Split('.')[0]
-
-Write-Output "Display Version: $catalogProductDisplayVersion"
-Write-Output "Line Major:      $catalogProductLineMajor"
+$majorVersion = $displayVersion.Split('.')[0]
+Write-Output "Visual Studio $majorVersion $releaseYear"

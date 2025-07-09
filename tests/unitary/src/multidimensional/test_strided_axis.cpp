7f321b09636ef8bf5cd90469897460b8064ed457
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-07-09 10:21:38 +0200

    Added test for exception

--- tests/unitary/src/multidimensional/test_strided_axis.cpp
+++ tests/unitary/src/multidimensional/test_strided_axis.cpp
@@ -291,11 +291,21 @@ TEST_CASE("apply_index should increment the offset as expected", "[strided_axis]
     REQUIRE( offset == initial_offset + index*axis.get_stride() );
 }
 
+TEST_CASE("apply_index should throw with an out of bounds index", "[strided_axis]")
+{
+    const std::ptrdiff_t initial_offset = 24;
+    const strided_axis axis(16, -5);
+    const std::size_t index = 16;
+
+    auto offset = initial_offset;
+    REQUIRE_THROWS_AS( apply_index(axis, offset, index), std::out_of_range );
+}
+
 TEST_CASE("apply_slice should increment the offset and modify the axis", "[strided_axis]")
 {
     const std::ptrdiff_t initial_offset = 24;
     const strided_axis initial_axis(16, -5);
-    const slice<std::size_t, std::size_t, std::ptrdiff_t> slice(2, 5, 3);
+    const dynamic_slice slice(2, 5, 3);
 
     auto axis = initial_axis;
     auto offset = initial_offset;
@@ -303,4 +313,15 @@ TEST_CASE("apply_slice should increment the offset and modify the axis", "[strid
     REQUIRE( offset == initial_offset + slice.get_start()*initial_axis.get_stride() );
     REQUIRE( axis.get_extent() == slice.get_count() );
     REQUIRE( axis.get_stride() == slice.get_step()*initial_axis.get_stride() );
+}
+
+TEST_CASE("apply_slice should throw with an out of range index", "[strided_axis]")
+{
+    const std::ptrdiff_t initial_offset = 24;
+    const strided_axis initial_axis(16, -5);
+    const dynamic_slice slice(12, 5, 3);
+
+    auto axis = initial_axis;
+    auto offset = initial_offset;
+    REQUIRE_THROWS_AS( apply_slice(axis, offset, slice), std::out_of_range );
 }

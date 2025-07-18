e2df64749602307ec4eb211cd748134ca81b8477
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-07-18 12:47:16 +0200

    Created base classes for unified memory (#202)
    Created `unified_buffer` and `unified_memory_allocator` that serve as
    intermediary base classes for jointly implementing `device_buffer`,
    `host_buffer`, `device_memory_allocator` and `host_memory_allcoator` in
    unified memory architectures
    
    ---------
    
    Co-authored-by: Martin Salinas <ssalinasmartin@gmail.com>

--- src/compute/cpu/cpu_unified_buffer.cpp
+++ src/compute/cpu/cpu_unified_buffer.cpp
@@ -2,31 +2,91 @@
 
 #include <xmipp4/core/compute/cpu/cpu_unified_buffer.hpp>
 
+#include <xmipp4/core/memory/align.hpp>
+#include <xmipp4/core/memory/aligned_alloc.hpp>
+#include <xmipp4/core/compute/cpu/cpu_device_queue.hpp>
+
+#include <stdexcept>
+
 namespace xmipp4
 {
 namespace compute
 {
 
-cpu_unified_buffer* cpu_unified_buffer::get_device_accessible_alias() noexcept
+cpu_unified_buffer::cpu_unified_buffer() noexcept
+    : m_size(0)
+    , m_data(nullptr)
 {
-    return this;
 }
 
-const cpu_unified_buffer* 
-cpu_unified_buffer::get_device_accessible_alias() const noexcept
+cpu_unified_buffer
+::cpu_unified_buffer(std::size_t size, std::size_t alignment)
+    : m_size(size)
+    , m_data(memory::aligned_alloc(memory::align_ceil(size, alignment), alignment))
 {
-    return this;
+    if(m_data == nullptr)
+    {
+        throw std::bad_alloc();
+    }
 }
 
-cpu_unified_buffer* cpu_unified_buffer::get_host_accessible_alias() noexcept
+cpu_unified_buffer
+::cpu_unified_buffer(cpu_unified_buffer &&other) noexcept
+    : m_size(other.m_size)
+    , m_data(nullptr)
 {
-    return this;
 }
 
-const cpu_unified_buffer* 
-cpu_unified_buffer::get_host_accessible_alias() const noexcept
+cpu_unified_buffer::~cpu_unified_buffer()
 {
-    return this;
+    reset();
+}
+
+cpu_unified_buffer& 
+cpu_unified_buffer
+::operator=(cpu_unified_buffer &&other) noexcept
+{
+    swap(other);
+    other.reset();
+    return *this;
+}
+
+void cpu_unified_buffer
+::swap(cpu_unified_buffer &other) noexcept
+{
+    std::swap(m_size, other.m_size);
+    std::swap(m_data, other.m_data);
+}
+
+void cpu_unified_buffer::reset() noexcept
+{
+    if (m_data)
+    {
+        memory::aligned_free(m_data);
+        m_size = 0;
+        m_data = nullptr;
+    }
+}
+
+std::size_t cpu_unified_buffer::get_size() const noexcept
+{
+    return m_size;
+}
+
+void* cpu_unified_buffer::get_data() noexcept
+{
+    return m_data;
+}
+
+const void* cpu_unified_buffer::get_data() const noexcept
+{
+    return m_data;
+}
+
+void cpu_unified_buffer::record_queue(device_queue& queue)
+{
+    dynamic_cast<cpu_device_queue&>(queue); // Check
+    // No-op
 }
 
 } // namespace compute

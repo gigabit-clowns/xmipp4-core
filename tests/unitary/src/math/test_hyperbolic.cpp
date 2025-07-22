1e5a5ea0f0e5ed686484c03dbfa0efc7591edbe3
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-07-22 16:58:24 +0200

    Minor touch

--- tests/unitary/src/math/test_hyperbolic.cpp
+++ tests/unitary/src/math/test_hyperbolic.cpp
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+#include <xmipp4/core/math/hyperbolic.hpp>
+
+#include <xmipp4/core/math/constants.hpp>
+
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_approx.hpp>
 #include <catch2/generators/catch_generators.hpp>
 #include <catch2/catch_template_test_macros.hpp>
-
-#include <xmipp4/core/math/hyperbolic.hpp>
-#include <xmipp4/core/math/constants.hpp>
 
 #include <limits>
 #include <cmath>

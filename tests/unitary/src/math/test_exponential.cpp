3bf55bc7d334b4b885a5acc340f8a45e0c9bf8a8
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-07-22 16:16:02 +0200

    Refactored exponential tests to make use of GENERATE and TEMPLATE_TEST_CASE

--- tests/unitary/src/math/test_exponential.cpp
+++ tests/unitary/src/math/test_exponential.cpp
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/catch_approx.hpp>
-
 #include <xmipp4/core/math/exponential.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 #include <map>
 #include <limits>
@@ -11,137 +13,161 @@
 
 using namespace xmipp4::math;
 
-TEST_CASE( "exp", "[math]" ) 
+TEMPLATE_TEST_CASE( "exp should produce correct numerical results", "[math]", float, double ) 
 {
-    const std::map<double, double> ground_truth = 
-    {
-        {-20.0, 2.061153622438558e-09},
-        {-10.0, 4.539992976248485e-05},
-        {-1.0, 0.367879441171442},
-        {0.0, 1.0},
-        {0.5, 1.648721270700128},
-        {10.0, 2.202646579480672e+04},
-    };
+    TestType x, expected;
+    std::tie(x, expected) = GENERATE(
+        table<TestType, TestType>({
+            {-20.0, 2.061153622438558e-09},
+            {-10.0, 4.539992976248485e-05},
+            {-1.0, 0.367879441171442},
+            {0.0, 1.0},
+            {0.5, 1.648721270700128},
+            {10.0, 2.202646579480672e+04},
+        })
+    );
 
-    for(const auto& sample : ground_truth)
-    {
-        REQUIRE( xmipp4::math::exp(sample.first) == Catch::Approx(sample.second) );
-        REQUIRE( xmipp4::math::exp(static_cast<float>(sample.first)) == Catch::Approx(sample.second) );
-    }
+    REQUIRE( xmipp4::math::exp(x) == Catch::Approx(expected) );
 }
 
-TEST_CASE( "exp2", "[math]" ) 
+TEMPLATE_TEST_CASE( "exp2 should produce correct numerical results", "[math]", float, double ) 
 {
-    const std::map<double, double> ground_truth = 
-    {
-        {-20.0, 9.536743164062500e-07},
-        {-10.0, 9.765625000000000e-04},
-        {-1.0, 0.5},
-        {0.0, 1.0},
-        {0.5, 1.414213562373095},
-        {10.0, 1024.0},
-    };
+    TestType x, expected;
+    std::tie(x, expected) = GENERATE(
+        table<TestType, TestType>({
+            {-20.0, 9.536743164062500e-07},
+            {-10.0, 9.765625000000000e-04},
+            {-1.0, 0.5},
+            {0.0, 1.0},
+            {0.5, 1.414213562373095},
+            {10.0, 1024.0},
+        })
+    );
 
-    for(const auto& sample : ground_truth)
-    {
-        REQUIRE( xmipp4::math::exp2(sample.first) == Catch::Approx(sample.second) );
-        REQUIRE( xmipp4::math::exp2(static_cast<float>(sample.first)) == Catch::Approx(sample.second) );
-    }
+    REQUIRE( xmipp4::math::exp2(x) == Catch::Approx(expected) );
 }
 
-TEST_CASE( "exp10", "[math]" ) 
+TEMPLATE_TEST_CASE( "exp10 should produce correct numerical results", "[math]", float, double ) 
 {
-    const std::map<double, double> ground_truth = 
-    {
-        {-20.0, 1e-20},
-        {-10.0, 1e-10},
-        {-1.0, 0.1},
-        {0.0, 1.0},
-        {0.5, 3.162277660168380},
-        {10.0, 1e10},
-    };
+    TestType x, expected;
+    std::tie(x, expected) = GENERATE(
+        table<TestType, TestType>({
+            {-20.0, 1e-20},
+            {-10.0, 1e-10},
+            {-1.0, 0.1},
+            {0.0, 1.0},
+            {0.5, 3.162277660168380},
+            {10.0, 1e10},
+        })
+    );
 
-    for(const auto& sample : ground_truth)
-    {
-        REQUIRE( xmipp4::math::exp10(sample.first) == Catch::Approx(sample.second) );
-        REQUIRE( xmipp4::math::exp10(static_cast<float>(sample.first)) == Catch::Approx(sample.second) );
-    }
+    REQUIRE( xmipp4::math::exp10(x) == Catch::Approx(expected) );
 }
 
-TEST_CASE( "log", "[math]" ) 
+TEMPLATE_TEST_CASE( "log should produce correct numerical results", "[math]", float, double ) 
 {
-    const std::map<double, double> ground_truth = 
-    {
-        {0.01, -4.605170185988091},
-        {0.1, -2.302585092994046},
-        {0.5, -0.693147180559945},
-        {1.0, 0.0},
-        {5.0, 1.609437912434100},
-        {10.0, 2.302585092994046},
-        {50.0, 3.912023005428146},
-        {1e6, 13.815510557964274},
-    };
+    TestType x, expected;
+    std::tie(x, expected) = GENERATE(
+        table<TestType, TestType>({
+            {0.01, -4.605170185988091},
+            {0.1, -2.302585092994046},
+            {0.5, -0.693147180559945},
+            {1.0, 0.0},
+            {5.0, 1.609437912434100},
+            {10.0, 2.302585092994046},
+            {50.0, 3.912023005428146},
+            {1e6, 13.815510557964274},
+        })
+    );
 
-    for(const auto& sample : ground_truth)
-    {
-        REQUIRE( xmipp4::math::log(sample.first) == Catch::Approx(sample.second) );
-        REQUIRE( xmipp4::math::log(static_cast<float>(sample.first)) == Catch::Approx(sample.second) );
-    }
-
-    REQUIRE( xmipp4::math::log(0.0) == -std::numeric_limits<double>::infinity() );
-    REQUIRE( xmipp4::math::log(0.0f) == -std::numeric_limits<float>::infinity() );
-    REQUIRE( std::isnan(xmipp4::math::log(-1.0)) );
-    REQUIRE( std::isnan(xmipp4::math::log(-1.0f)) );
+    REQUIRE( xmipp4::math::log(x) == Catch::Approx(expected) );
 }
 
-TEST_CASE( "log2", "[math]" ) 
+TEMPLATE_TEST_CASE( "log with 0 as input should produce infinity", "[math]", float, double ) 
 {
-    const std::map<double, double> ground_truth = 
-    {
-        {0.01, -6.643856189774724},
-        {0.1, -3.321928094887362},
-        {0.5, -1.0},
-        {1.0, 0.0},
-        {2.0, 1.0},
-        {5.0, 2.321928094887362},
-        {10.0, 3.321928094887362},
-        {1e6, 19.931568569324174},
-    };
-
-    for(const auto& sample : ground_truth)
-    {
-        REQUIRE( xmipp4::math::log2(sample.first) == Catch::Approx(sample.second) );
-        REQUIRE( xmipp4::math::log2(static_cast<float>(sample.first)) == Catch::Approx(sample.second) );
-    }
-
-    REQUIRE( xmipp4::math::log2(0.0) == -std::numeric_limits<double>::infinity() );
-    REQUIRE( xmipp4::math::log2(0.0f) == -std::numeric_limits<float>::infinity() );
-    REQUIRE( std::isnan(xmipp4::math::log2(-1.0)) );
-    REQUIRE( std::isnan(xmipp4::math::log2(-1.0f)) );
+    const TestType value = GENERATE(+0.0, -0.0);
+    REQUIRE( xmipp4::math::log(value) == -std::numeric_limits<TestType>::infinity() );
 }
 
-TEST_CASE( "log10", "[math]" ) 
+TEMPLATE_TEST_CASE( "log with negative values as input should produce NaN", "[math]", float, double ) 
 {
-    const std::map<double, double> ground_truth = 
-    {
-        {0.01, -2.0},
-        {0.1, -1.0},
-        {0.5, -0.301029995663981},
-        {1.0, 0.0},
-        {2.0, 0.301029995663981},
-        {5.0, 0.698970004336019},
-        {10.0, 1.0},
-        {1e6, 6.0},
-    };
-
-    for(const auto& sample : ground_truth)
-    {
-        REQUIRE( xmipp4::math::log10(sample.first) == Catch::Approx(sample.second) );
-        REQUIRE( xmipp4::math::log10(static_cast<float>(sample.first)) == Catch::Approx(sample.second) );
-    }
-
-    REQUIRE( xmipp4::math::log10(0.0) == -std::numeric_limits<double>::infinity() );
-    REQUIRE( xmipp4::math::log10(0.0f) == -std::numeric_limits<float>::infinity() );
-    REQUIRE( std::isnan(xmipp4::math::log10(-1.0)) );
-    REQUIRE( std::isnan(xmipp4::math::log10(-1.0f)) );
+    const TestType value = GENERATE(-1.0, -10.0, -100.0);
+    REQUIRE( std::isnan(xmipp4::math::log(value)) );
 }
+
+TEMPLATE_TEST_CASE( "log2 should produce correct numerical results", "[math]", float, double ) 
+{
+    TestType x, expected;
+    std::tie(x, expected) = GENERATE(
+        table<TestType, TestType>({
+            {0.01, -6.643856189774724},
+            {0.1, -3.321928094887362},
+            {0.5, -1.0},
+            {1.0, 0.0},
+            {2.0, 1.0},
+            {5.0, 2.321928094887362},
+            {10.0, 3.321928094887362},
+            {1e6, 19.931568569324174},
+        })
+    );
+
+    REQUIRE( xmipp4::math::log2(x) == Catch::Approx(expected) );
+}
+
+TEMPLATE_TEST_CASE( "log2 with 0 as input should produce infinity", "[math]", float, double ) 
+{
+    const TestType value = GENERATE(+0.0, -0.0);
+    REQUIRE( xmipp4::math::log2(value) == -std::numeric_limits<TestType>::infinity() );
+}
+
+TEMPLATE_TEST_CASE( "log2 with negative values as input should produce NaN", "[math]", float, double ) 
+{
+    const TestType value = GENERATE(-1.0, -10.0, -100.0);
+    REQUIRE( std::isnan(xmipp4::math::log2(value)) );
+}
+
+TEMPLATE_TEST_CASE( "log10 should produce correct numerical results", "[math]", float, double ) 
+{
+    TestType x, expected;
+    std::tie(x, expected) = GENERATE(
+        table<TestType, TestType>({
+            {0.01, -2.0},
+            {0.1, -1.0},
+            {0.5, -0.301029995663981},
+            {1.0, 0.0},
+            {2.0, 0.301029995663981},
+            {5.0, 0.698970004336019},
+            {10.0, 1.0},
+            {1e6, 6.0},
+        })
+    );
+
+    REQUIRE( xmipp4::math::log10(x) == Catch::Approx(expected) );
+}
+
+
+TEMPLATE_TEST_CASE( "log10 with 0 as input should produce infinity", "[math]", float, double ) 
+{
+    const TestType value = GENERATE(+0.0, -0.0);
+    REQUIRE( xmipp4::math::log10(value) == -std::numeric_limits<TestType>::infinity() );
+}
+
+TEMPLATE_TEST_CASE( "log10 with negative values as input should produce NaN", "[math]", float, double ) 
+{
+    const TestType value = GENERATE(-1.0, -10.0, -100.0);
+    REQUIRE( std::isnan(xmipp4::math::log10(value)) );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

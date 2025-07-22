6a0d17c45f01abaf47bf9809956229a402ee57e5
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-07-22 17:21:38 +0200

    Refactored test abs to make use of GENERATE and TEMPLATE_TEST_CASE

--- tests/unitary/src/math/test_abs.cpp
+++ tests/unitary/src/math/test_abs.cpp
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-#include <catch2/catch_test_macros.hpp>
-
 #include <xmipp4/core/math/abs.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 #include <limits>
 #include <cstdint>
@@ -10,26 +12,21 @@
 using namespace xmipp4::math;
 
 
-TEST_CASE( "abs", "[math]" ) 
+TEMPLATE_TEST_CASE( "abs should return the same result when input is positive", "[math]", float, double, int, unsigned int )
 {
-    REQUIRE( abs(static_cast<uint8_t>(1)) == 1 );
-    REQUIRE( abs(static_cast<uint8_t>(255)) == 255 );
-    REQUIRE( abs(static_cast<uint64_t>(1)) == 1 );
-    REQUIRE( abs(static_cast<uint64_t>(545465)) == 545465 );
+    TestType input = GENERATE(0, 1, 255, 1000, 123456789);
+    REQUIRE( xmipp4::math::abs(input) == input );
+}
 
-    REQUIRE( abs(static_cast<int8_t>(0)) == 0 );
-    REQUIRE( abs(static_cast<int8_t>(1)) == 1 );
-    REQUIRE( abs(static_cast<int8_t>(-128)) == 128 );
-    REQUIRE( abs(static_cast<int64_t>(0)) == 0 );
-    REQUIRE( abs(static_cast<int64_t>(1)) == 1 );
-    REQUIRE( abs(static_cast<int64_t>(-643214)) == 643214 );
+TEMPLATE_TEST_CASE( "abs should return the same negated result when input is negative", "[math]", float, double, int )
+{
+    TestType input = GENERATE(0, 1, 255, 1000, 123456789);
+    REQUIRE( xmipp4::math::abs(-input) == input );
+}
 
-    REQUIRE( abs(0.0f) == 0.0f );
-    REQUIRE( abs(1.0f) == 1.0f );
-    REQUIRE( abs(-1.0f) == 1.0f );
-    REQUIRE( abs(-std::numeric_limits<float>::infinity()) == std::numeric_limits<float>::infinity() );
-    REQUIRE( abs(0.0) == 0.0 );
-    REQUIRE( abs(1.0) == 1.0 );
-    REQUIRE( abs(-1.0) == 1.0 );
-    REQUIRE( abs(-std::numeric_limits<double>::infinity()) == std::numeric_limits<double>::infinity() );
+TEMPLATE_TEST_CASE( "abs should return inf when input is plus or minus inf", "[math]", float, double )
+{
+    const auto input = std::numeric_limits<TestType>::infinity();
+    REQUIRE( xmipp4::math::abs(input) == input );
+    REQUIRE( xmipp4::math::abs(-input) == input );
 }

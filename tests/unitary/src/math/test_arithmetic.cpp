83c115f26006438697929093a349258895db86fc
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-07-22 16:02:36 +0200

    Refactored arithmetic tests to make use of TEMPLATE_TEST_CASE and GENERATE

--- tests/unitary/src/math/test_arithmetic.cpp
+++ tests/unitary/src/math/test_arithmetic.cpp
@@ -2,26 +2,34 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_approx.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/catch_template_test_macros.hpp>
 
 #include <xmipp4/core/math/arithmetic.hpp>
 
 using namespace xmipp4::math;
 
 
-TEST_CASE( "multiply_add", "[math]" ) 
+TEMPLATE_TEST_CASE("multiply_add should produce correct numerical results", "[math]", float, double)
 {
-    REQUIRE( xmipp4::math::multiply_add(2.31, 1.35, 2.21) == Catch::Approx(5.3285) );
-    REQUIRE( xmipp4::math::multiply_add(2.31f, 1.35f, 2.21f) == Catch::Approx(5.3285f) );
-
-    REQUIRE( xmipp4::math::multiply_add(-2.5252, 5.23565, 73.2121) == Catch::Approx(59.99103662) );
-    REQUIRE( xmipp4::math::multiply_add(-2.5252f, 5.23565f, 73.2121f) == Catch::Approx(59.99103662) );
+    TestType a, b, c, expected;
+    std::tie(a, b, c, expected) = GENERATE(
+        table<TestType, TestType, TestType, TestType>({
+            {2.31, 1.35, 2.21, 5.3285},
+            {-2.5252, 5.23565, 73.2121, 59.99103662}
+        })
+    );
+    REQUIRE(multiply_add(a, b, c) == Catch::Approx(expected));
 }
 
-TEST_CASE( "mod", "[math]" ) 
+TEMPLATE_TEST_CASE("mod should produce correct numerical results", "[math]", float, double)
 {
-    REQUIRE( xmipp4::math::mod(2.31, 1.35) == Catch::Approx(0.96) );
-    REQUIRE( xmipp4::math::mod(2.31f, 1.35f) == Catch::Approx(0.96) );
-    
-    REQUIRE( xmipp4::math::mod(2.31, -1.35) == Catch::Approx(0.96) );
-    REQUIRE( xmipp4::math::mod(2.31f, -1.35f) == Catch::Approx(0.96) );
+    TestType a, b, expected;
+    std::tie(a, b, expected) = GENERATE(
+        table<TestType, TestType, TestType>({
+            {2.31, 1.35, 0.96},
+        })
+    );
+    REQUIRE(mod(a, b) == Catch::Approx(expected));
+    REQUIRE(mod(-a, b) == Catch::Approx(-expected));
 }

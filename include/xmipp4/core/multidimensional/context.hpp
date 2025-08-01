f3a346d85e1cdc0de81cc86689c2093fc539aba2
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-08-01 21:39:05 +0200

    Added documentation to context

--- include/xmipp4/core/multidimensional/context.hpp
+++ include/xmipp4/core/multidimensional/context.hpp
@@ -14,6 +14,13 @@ class device_queue;
 
 } // namespace
 
+/**
+ * @brief Defines the environment used in computations.
+ * 
+ * This class holds the set of helper classes used in computations. Is is 
+ * generally provided to operators in order to define execution policies.
+ * 
+ */
 namespace multidimensional
 {
 
@@ -31,24 +38,81 @@ public:
     context& operator=(const context &other) = delete;
     context& operator=(context &&other) noexcept;
 
+    /**
+     * @brief Set the current device for this context.
+     * 
+     * @param device The device for this context. Use nullptr for none.
+     */
     void set_device(std::shared_ptr<compute::device> device) noexcept;
 
+    /**
+     * @brief Get the current device for this context.
+     * 
+     * @return compute::device* The current device.
+     */
     compute::device* get_device() const noexcept;
 
+    /**
+     * @brief Set the current device_queue for this context.
+     * 
+     * @param queue The device_queue for this context. Use nullptr for none.
+     */
     void set_device_queue(std::shared_ptr<compute::device_queue> queue) noexcept;
 
+    /**
+     * @brief Get the current device for this context.
+     * 
+     * @return compute::device_queue* The current device_queue.
+     */
     compute::device_queue* get_device_queue() const noexcept;
 
+    /**
+     * @brief Set the current allocator for this context.
+     * 
+     * @param allocator The allocator for this context. Use nullptr for none.
+     */
     void set_allocator(std::shared_ptr<allocator> allocator) noexcept;
 
+    /**
+     * @brief Get the current allocator for this context.
+     * 
+     * @return allocator* The current device_queue.
+     */
     allocator* get_allocator() const noexcept;
 
+    /**
+     * @brief Set the current scratch allocator for this context.
+     * 
+     * @param allocator The scratch allocator for this context. 
+     * Use nullptr for none.
+     * 
+     */
     void set_scratch_allocator(std::shared_ptr<allocator> allocator) noexcept;
 
+    /**
+     * @brief Get the current scratch allocator for this context.
+     * 
+     * Unlike the allocator provided by get_allocator, the allocator returned
+     * by this method is meant to be used for transient allocations used to hold 
+     * intermediate results during computations. It is fine if the scratch 
+     * allocator aliases the "normal" allocator.
+     * 
+     * @return allocator* The current scratch allocator.
+     */
     allocator* get_scratch_allocator() const noexcept;
 
+    /**
+     * @brief Set the current layout policy for this context.
+     * 
+     * @param policy The layout policy for this context. Use nullptr for none
+     */
     void set_layout_policy(std::shared_ptr<strided_layout_policy> policy) noexcept;
 
+    /**
+     * @brief Get the current layout policy for this context.
+     * 
+     * @return strided_layout_policy* The current layout policy.
+     */
     strided_layout_policy* get_layout_policy() const noexcept;
 
 private:

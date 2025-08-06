cb51138c8675ad57f9c881ef4816640f42d6d865
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-08-06 10:58:47 +0200

    Added documentation to storage

--- include/xmipp4/core/multidimensional/storage.hpp
+++ include/xmipp4/core/multidimensional/storage.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <cstddef>
+#include <typeinfo>
 
 namespace xmipp4 
 {
@@ -19,10 +20,29 @@ namespace multidimensional
 class storage
 {
 public:
-    virtual compute::device* get_device() const noexcept = 0;
+    /**
+     * @brief Get the size in bytes for this buffer.
+     * 
+     * @return std::size_t Size in bytes.
+     */
     virtual std::size_t get_size() const noexcept = 0;
-    virtual void* get_data_ptr() noexcept = 0;
-    virtual const void* get_data_ptr() const noexcept = 0;
+
+    /**
+     * @brief Get the backend key for dispatching operations with this buffer.
+     * 
+     * The backend key is usually obtained by declaring a stateless tag
+     * and returning typeid(my_backend_tag)
+     * 
+     * @return const std::type_info& The backend key.
+     */
+    virtual const std::type_info& get_backend_key() const noexcept;
+
+    /**
+     * @brief Get the device where this buffer is stored. 
+     * 
+     * @return compute::device& The device where the buffer is stored.
+     */
+    virtual compute::device& get_device() const noexcept = 0;
 
 };
 

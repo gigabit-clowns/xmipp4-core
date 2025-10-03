e9c0e8a819442b9933769c8b02b4ac2a211c9f9f
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-10-03 20:41:45 +0200

    Minor refactor in broadcast

--- src/multidimensional/broadcast.cpp
+++ src/multidimensional/broadcast.cpp
@@ -38,6 +38,18 @@ bool broadcast_extent(std::size_t &extent1, std::size_t &extent2)
     return true;
 }
 
+static
+void pad_extents(
+    std::vector<std::size_t> &extents, 
+    std::size_t target_size
+)
+{
+    XMIPP4_ASSERT( extents.size() < target_size );
+
+    const auto padding = target_size - extents.size();
+    extents.insert(extents.cbegin(), padding, 1U);
+}
+
 void broadcast_extents(
     std::vector<std::size_t> &extents1, 
     std::vector<std::size_t> &extents2 
@@ -45,14 +57,12 @@ void broadcast_extents(
 {
     if (extents1.size() < extents2.size())
     {
-        const auto padding = extents2.size() - extents1.size();
-        extents1.insert(extents1.cbegin(), padding, 1U);
+        pad_extents(extents1, extents2.size());
     }
     
     if (extents2.size() < extents1.size())
     {
-        const auto padding = extents1.size() - extents2.size();
-        extents2.insert(extents2.cbegin(), padding, 1U);
+        pad_extents(extents2, extents1.size());
     }
 
     XMIPP4_ASSERT( extents1.size() == extents2.size() );

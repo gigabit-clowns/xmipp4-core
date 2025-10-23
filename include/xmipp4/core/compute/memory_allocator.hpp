f824d2c2d35dc098eded7a4d4c4cfc32ab4e097d
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-10-23 09:39:27 +0200

    Added doc to allocator

--- include/xmipp4/core/compute/memory_allocator.hpp
+++ include/xmipp4/core/compute/memory_allocator.hpp
@@ -17,6 +17,13 @@ class memory_resource;
 
 
 
+/**
+ * @brief Abstract class for memory allocators.
+ * 
+ * Instances of memory allocators can be obtained from memory_resource-s. Their
+ * purpose is to allocate buffers on the associated memory_resource.
+ * 
+ */
 class XMIPP4_CORE_API memory_allocator
 {
 public:
@@ -28,9 +35,28 @@ public:
     memory_allocator& operator=(const memory_allocator &other) = default;
     memory_allocator& operator=(memory_allocator &&other) = default;
 
+    /**
+     * @brief Get the associated memory_resource.
+     * 
+     * @return memory_resource& The memory resource where this allocator
+     * allocates buffers.
+     */
     virtual
     memory_resource& get_memory_resource() const noexcept = 0;
 
+    /**
+     * @brief Allocate a buffer of given size and alignment.
+     * 
+     * @param size Size in bytes.
+     * @param alignment Alignment in bytes. Must be a power of two.
+     * @param queue Optional device_queue where the allocation is dispatched.
+     * If provided, the allocation may be asynchronous with respect to the
+     * host. The queue must be associated to the same device as the memory
+     * resource targeted by this allocator. When not provided, the allocation
+     * is synchronous, which means that the buffer is ready to be used when this
+     * method returns.
+     * @return std::shared_ptr<buffer> The newly allocated buffer.
+     */
     virtual
     std::shared_ptr<buffer> allocate(
         std::size_t size, 

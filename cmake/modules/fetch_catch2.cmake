560899b0bfd5f27a4fd0b5eb6110d7de500b0cf9
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-11-25 23:30:57 +0100

    Added fetch scripts for Catch2 and trompeloeil

---
+++ cmake/modules/fetch_catch2.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 3.12)
+
+include(FetchContent)
+
+function(fetch_catch2)
+	set(options)
+	set(oneValueArgs VERSION)
+	set(multiValueArgs)
+	cmake_parse_arguments(PARSE_ARGV 0 arg
+		"${options}" "${oneValueArgs}" "${multiValueArgs}"
+	)
+
+	cmake_policy(SET CMP0135 NEW) # To avoid warnings
+	FetchContent_Declare(
+		Catch2
+		URL https://github.com/catchorg/Catch2/archive/refs/tags/v${arg_VERSION}.tar.gz
+	)
+	FetchContent_MakeAvailable(Catch2)
+endfunction()

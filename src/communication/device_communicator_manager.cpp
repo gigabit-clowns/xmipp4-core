6dc83cb7fff5520c3d8a3268e2d4d7168b9ebb41
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-11-25 12:41:39 +0100

    Added implementation to device_communicator_manager

--- src/communication/device_communicator_manager.cpp
+++ src/communication/device_communicator_manager.cpp
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-#include <xmipp4/core/communication/host_communicator_manager.hpp>
+#include <xmipp4/core/communication/device_communicator_manager.hpp>
 
 #include <xmipp4/core/exceptions/invalid_operation_error.hpp>
 #include <xmipp4/core/platform/assert.hpp>
 
 #include "../find_most_suitable_backend.hpp"
-#include "dummy/dummy_host_communicator_backend.hpp"
 
 #include <vector>
 #include <algorithm>
@@ -16,11 +15,11 @@ namespace xmipp4
 namespace communication
 {
 
-class host_communicator_manager::implementation
+class device_communicator_manager::implementation
 {
 public:
 	bool register_backend(
-		std::unique_ptr<host_communicator_backend> backend
+		std::unique_ptr<device_communicator_backend> backend
 	)
 	{
 		if (!backend)
@@ -38,7 +37,11 @@ public:
 		return inserted;
 	}
 
-	std::shared_ptr<host_communicator> create_world_communicator() const
+	void create_world_communicators(
+		span<hardware::device*> devices,
+		const std::shared_ptr<host_communicator> &host_communicator,
+		std::vector<std::shared_ptr<device_communicator>> &out
+	) const
 	{
 		const auto backend = find_most_suitable_backend(
 			m_backends.cbegin(), m_backends.cend(),
@@ -52,17 +55,21 @@ public:
 		if (backend == m_backends.cend())
 		{
 			throw invalid_operation_error(
-				"There is no available device host_communicator backend"
+				"There is no available device device_communicator backend"
 			);
 		}
 
-		return backend->second->create_world_communicator();
+		return backend->second->create_world_communicators(
+			devices,
+			host_communicator,
+			out
+		);
 	}
 
 private:
 	using backend_map = 
 		std::unordered_map<std::string, 
-		std::unique_ptr<host_communicator_backend>
+		std::unique_ptr<device_communicator_backend>
 	>;
 
 	backend_map m_backends;
@@ -70,25 +77,27 @@ private:
 
 
 
-host_communicator_manager::host_communicator_manager() = default;
+device_communicator_manager::device_communicator_manager() = default;
 
-host_communicator_manager::~host_communicator_manager() = default;
+device_communicator_manager::~device_communicator_manager() = default;
 
-void host_communicator_manager::register_builtin_backends()
+void device_communicator_manager::register_builtin_backends()
 {
-	dummy_host_communicator_backend::register_at(*this);
 }
 
-bool host_communicator_manager::register_backend(
-	std::unique_ptr<host_communicator_backend> backend
+bool device_communicator_manager::register_backend(
+	std::unique_ptr<device_communicator_backend> backend
 )
 {
 	create_implementation_if_null();
 	return m_implementation->register_backend(std::move(backend));
 }
 
-std::shared_ptr<host_communicator> 
-host_communicator_manager::create_world_communicator() const
+void device_communicator_manager::create_world_communicators(
+	span<hardware::device*> devices,
+	const std::shared_ptr<host_communicator> &host_communicator,
+	std::vector<std::shared_ptr<device_communicator>> &out
+) const
 {
 	if (!m_implementation)
 	{
@@ -97,10 +106,14 @@ host_communicator_manager::create_world_communicator() const
 		);
 	}
 
-	return m_implementation->create_world_communicator();
+	return m_implementation->create_world_communicators(
+		devices,
+		host_communicator,
+		out
+	);
 }
 
-void host_communicator_manager::create_implementation_if_null()
+void device_communicator_manager::create_implementation_if_null()
 {
 	if (!m_implementation)
 	{

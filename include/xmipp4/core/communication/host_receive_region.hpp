5916f54f3248a9d1150fea745afec6eb79faca43
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-11-27 19:21:35 +0100

    Added documentation

--- include/xmipp4/core/communication/host_receive_region.hpp
+++ include/xmipp4/core/communication/host_receive_region.hpp
@@ -12,16 +12,39 @@ namespace xmipp4
 namespace communication
 {
 
+/**
+ * @brief Representation of a dynamically typed host memory region.
+ * 
+ */
 class host_receive_region
 {
 public:
+	/**
+	 * @brief Construct an empty region.
+	 *
+	 */
 	XMIPP4_CONSTEXPR
 	host_receive_region() noexcept;
 
+	/**
+	 * @brief Construct a memory region from a typed pointer and element count.
+	 * 
+	 * @tparam T Type of the elements.
+	 * @param data Pointer to the elements.
+	 * @param count Number of elements.
+	 */
 	template <typename T>
 	XMIPP4_CONSTEXPR
 	host_receive_region(T* data, std::size_t count) noexcept;
 
+	/**
+	 * @brief Construct a memory region from a untyped pointer, dynamic type and
+	 * element count
+	 * 
+	 * @param data Untyped pointer.
+	 * @param data_type Type of the elements pointed by the pointer.
+	 * @param count Number of elements.
+	 */
 	XMIPP4_CONSTEXPR
 	host_receive_region(
 		void *data, 
@@ -36,10 +59,25 @@ public:
 	host_receive_region& operator=(const host_receive_region &other) = default;
 	host_receive_region& operator=(host_receive_region &&other) = default;
 
+	/**
+	 * @brief Get an untyped pointer to the data.
+	 * 
+	 * @return void* the pointer.
+	 */
 	XMIPP4_CONSTEXPR void* get_data() const noexcept;
 
+	/**
+	 * @brief Get the type of the elements.
+	 * 
+	 * @return numerical_type the type.
+	 */
 	XMIPP4_CONSTEXPR numerical_type get_data_type() const noexcept;
 
+	/**
+	 * @brief Get the number of elements.
+	 * 
+	 * @return std::size_t element count.
+	 */
 	XMIPP4_CONSTEXPR std::size_t get_count() const noexcept;
 
 private:
--- include/xmipp4/core/communication/host_send_receive_regions.hpp
+++ include/xmipp4/core/communication/host_send_receive_regions.hpp
@@ -12,12 +12,30 @@ namespace xmipp4
 namespace communication
 {
 
+/**
+ * @brief Represents an equally type and sized pair of a dynamically typed host 
+ * memory region.
+ * 
+ */
 class host_send_receive_regions
 {
 public:
+	/**
+	 * @brief Construct an empty region pair.
+	 *
+	 */
 	XMIPP4_CONSTEXPR
 	host_send_receive_regions() noexcept;
 
+	/**
+	 * @brief Construct the regions from their respective typed pointers and 
+	 * count.
+	 * 
+	 * @tparam T Type of the elements.
+	 * @param send_data Read only pointer to the data to be sent.
+	 * @param receive_data Pointer to the data to be received.
+	 * @param count Number of elements.
+	 */
 	template <typename T>
 	XMIPP4_CONSTEXPR
 	host_send_receive_regions(
@@ -26,6 +44,15 @@ public:
 		std::size_t count
 	) noexcept;
 
+	/**
+	 * @brief Construct the regions from their respective untyped pointers, 
+	 * dynamic type and count.
+	 * 
+	 * @param send_data Read only pointer to the data to be sent.
+	 * @param receive_data Pointer to the data to be received.
+	 * @param data_type Type of the elements pointed by the pointers.
+	 * @param count Number of elements.
+	 */
 	XMIPP4_CONSTEXPR
 	host_send_receive_regions(
 		const void *send_data, 
@@ -34,16 +61,32 @@ public:
 		std::size_t count
 	) noexcept;
 
+	/**
+	 * @brief Construct an aliasing pair of regions from a typed pointer.
+	 * 
+	 * @tparam T Type of the elements.
+	 * @param send_receive_data Pointer to the data to be sent and received.
+	 * @param count Number of elements.
+	 */
 	template <typename T>
 	XMIPP4_CONSTEXPR
 	host_send_receive_regions(T *send_recv_data, std::size_t count) noexcept;
 
+	/**
+	 * @brief Construct an aliasing pair of regions from a untyped pointer and
+	 * dynamic type and count.
+	 * 
+	 * @param send_receive_data Pointer to the data to be sent and received.
+	 * @param data_type Type of the elements pointed by the pointers.
+	 * @param count Number of elements.
+	 */
 	XMIPP4_CONSTEXPR
 	host_send_receive_regions(
 		void *send_recv_data, 
 		numerical_type data_type, 
 		std::size_t count
 	) noexcept;
+
 	host_send_receive_regions(const host_send_receive_regions &other) = default;
 	host_send_receive_regions(host_send_receive_regions &&other) = default;
 	~host_send_receive_regions() = default;
@@ -53,12 +96,33 @@ public:
 	host_send_receive_regions& 
 	operator=(host_send_receive_regions &&other) = default;
 
+	/**
+	 * @brief Get an untype pointer to the data to be sent.
+	 * 
+	 * @return const void* Pointer to the data to be sent.
+	 */
 	XMIPP4_CONSTEXPR const void* get_send_data() const noexcept;
 
+	/**
+	 * @brief Get an untype pointer to the data to be received.
+	 * 
+	 * @return void* Pointer to the data to be received.
+	 */
 	XMIPP4_CONSTEXPR void* get_receive_data() const noexcept;
 
+	/**
+	 * @brief Get the data type.
+	 * 
+	 * @return numerical_type The data type.
+	 */
 	XMIPP4_CONSTEXPR numerical_type get_data_type() const noexcept;
 
+	/**
+	 * @brief Get the number of elements referenced by the send and receive
+	 * regions.
+	 * 
+	 * @return std::size_t The element count.
+	 */
 	XMIPP4_CONSTEXPR std::size_t get_count() const noexcept;
 
 private:

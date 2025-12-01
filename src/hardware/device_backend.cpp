61d8f35059de45dca77514f49b9b97eb97a2e172
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-12-01 22:48:16 +0100

    Added anchor .cpp files in hardware (#287)
    Trying to fix missing symbols when importing from CUDA plugin

---
+++ src/hardware/device_backend.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include <xmipp4/core/hardware/device_backend.hpp>
+
+namespace xmipp4
+{
+namespace hardware
+{
+
+device_backend::device_backend() noexcept = default;
+device_backend::~device_backend() = default;
+
+} // namespace hardware
+} // namespace xmipp4
---
+++ src/hardware/memory_transfer.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include <xmipp4/core/hardware/memory_transfer.hpp>
+
+namespace xmipp4
+{
+namespace hardware
+{
+
+memory_transfer::memory_transfer() noexcept = default;
+memory_transfer::~memory_transfer() = default;
+
+} // namespace hardware
+} // namespace xmipp4

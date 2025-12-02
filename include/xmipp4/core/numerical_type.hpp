52df6b6b5c046f5935f25e792a32a3f4f91c9ed7
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-12-02 21:01:03 +0100

    Added char8 numerical_type (#290)
    Added `char8` `numerical_type` to allow using strings with
    `host_communicator`. Removing `brain_float16` for now, Removing most
    `switch` statements in `numerical_type.inl`, including the horrible LUT.

--- include/xmipp4/core/numerical_type.hpp
+++ include/xmipp4/core/numerical_type.hpp
@@ -20,6 +20,8 @@ enum class numerical_type
 {
 	unknown = -1,
 
+	char8,
+
 	int8,
 	uint8,
 	int16,
@@ -30,7 +32,6 @@ enum class numerical_type
 	uint64,
 
 	float16,
-	brain_float16,
 	float32,
 	float64,
 
@@ -42,6 +43,52 @@ enum class numerical_type
 
 	count
 };
+
+/**
+ * @brief Convert static types to numerical_type.
+ * 
+ * The numerical_type of the provided static type is stored as 
+ * numerical_type_of<T>::value
+ * 
+ * @tparam T The static type to be converted.
+ */
+template <typename T>
+struct numerical_type_of;
+
+/**
+ * @brief Helper class to pass type information to a function.
+ * 
+ * Eventually to be replaced with std::type_identity in C++20.
+ * 
+ * @tparam T Type passed.
+ */
+template <typename T>
+class type_tag
+{
+public:
+	using type = T;
+
+};
+
+/**
+ * @brief Dispatch a function with the numerical types.
+ * 
+ * This function dispatches a set of numerical_type-s and calls the provided
+ * visitor with the dispatched types wrapped around a type_tag. In case a type
+ * fails conversion to a C++ type, type_tag<void> is passed.
+ * 
+ * @tparam F Functor to be dispatched.
+ * @tparam Types any amount of numeric_type-s
+ * @param visitor Function to be called with the dispatched types.
+ * @param types Types to be dispatched.
+ * @return auto The result of calling visitor(types...)
+ * 
+ * @see numerical_type
+ * @see type_tag
+ */
+template <typename F, typename... Types>
+XMIPP4_CONSTEXPR
+auto visit(F&& visitor, Types&& ...types);
 
 /**
  * @brief Get the size of the numerical type.
@@ -109,12 +156,12 @@ XMIPP4_CONSTEXPR numerical_type make_complex(numerical_type type) noexcept;
 /**
  * @brief Get the common type of two numerical types.
  * 
- * @param x First numerical type.
- * @param y Second numerical type.
+ * @param type1 First numerical type.
+ * @param type2 Second numerical type.
  * @return numerical_type The common type.
  */
 XMIPP4_CONSTEXPR 
-numerical_type common_type(numerical_type x, numerical_type y) noexcept;
+numerical_type common_type(numerical_type type1, numerical_type type2) noexcept;
 
 XMIPP4_CONSTEXPR 
 const char* to_string(numerical_type type) noexcept;

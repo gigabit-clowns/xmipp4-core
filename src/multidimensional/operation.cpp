5a66730a7902f85d2e5c269fe87eedc512654d20
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-12-05 16:59:45 +0100

    Created cpp files for headers

---
+++ src/multidimensional/operation.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include <xmipp4/core/multidimensional/operation.hpp>
+
+namespace xmipp4 
+{
+namespace multidimensional
+{
+
+operation::operation() noexcept = default;
+operation::~operation() = default;
+
+} // namespace multidimensional
+} // namespace xmipp4
---
+++ src/multidimensional/operation_schema.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include <xmipp4/core/multidimensional/operation_schema.hpp>
+
+namespace xmipp4 
+{
+namespace multidimensional
+{
+
+operation_schema::operation_schema() noexcept = default;
+operation_schema::~operation_schema() = default;
+
+} // namespace multidimensional
+} // namespace xmipp4

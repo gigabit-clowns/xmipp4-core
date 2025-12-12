fe5eaeaff3079bca8ceb598a2c4e0e7339f3ee6a
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-12-12 14:49:52 +0100

    Fixing tabs

--- src/multidimensional/strided_layout_implementation.hpp
+++ src/multidimensional/strided_layout_implementation.hpp
@@ -28,8 +28,8 @@ public:
 	strided_layout_implementation(
 		const strided_axis_vector_type &axes,
 		std::ptrdiff_t offset 
-	);   
-	
+	);
+
 	bool operator==(const strided_layout_implementation &other) const noexcept;
 
 	std::size_t get_rank() const noexcept;

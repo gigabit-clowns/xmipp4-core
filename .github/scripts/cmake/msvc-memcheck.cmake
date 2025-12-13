ba4ba4ae18ce3a6ad67b56f569a39942e78211ed
Martin Salinas <ssalinasmartin@gmail.com>
2025-12-13 14:35:29 +0100

    Offloaded composites to .github repository

--- .github/scripts/cmake/msvc-memcheck.cmake
+++
@@ -1,3 +0,0 @@
-# Force MSVC debug-friendly flags for memcheck (needed by Dr. Memory)
-set(CMAKE_CXX_FLAGS "/Zi /MT /EHsc /Oy- /Ob0" CACHE STRING "MSVC flags for memcheck")
-set(CMAKE_C_FLAGS "/Zi /MT /EHsc /Oy- /Ob0" CACHE STRING "MSVC flags for memcheck")

10e88590e44527dc7a0b6d5a77ae78792966967d
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-12-15 19:14:57 +0100

    Tabs

--- include/xmipp4/core/multidimensional/kernel_builder.hpp
+++ include/xmipp4/core/multidimensional/kernel_builder.hpp
@@ -38,7 +38,7 @@ public:
 	kernel_builder(const kernel_builder &other) = default;
 	kernel_builder(kernel_builder &&other) = default;
 	XMIPP4_CORE_API
-    virtual ~kernel_builder();
+	virtual ~kernel_builder();
 
 	kernel_builder& operator=(const kernel_builder &other) = default;
 	kernel_builder& operator=(kernel_builder &&other) = default;
@@ -81,8 +81,8 @@ public:
 	 * @return std::shared_ptr<kernel> The executable kernel suited for the
 	 * requested launch parameters.
 	 */
-    virtual 
-    std::shared_ptr<kernel> build(
+	virtual 
+	std::shared_ptr<kernel> build(
 		const operation &operation,
 		span<const array_descriptor> descriptors,
 		hardware::device &device

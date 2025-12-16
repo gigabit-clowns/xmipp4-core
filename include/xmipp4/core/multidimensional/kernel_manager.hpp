37af2cecc30940d2fc106ee1691421abd54c650c
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-12-16 12:21:52 +0100

    Implemented kernel related classes (#307)
    - `kernel`: Abstract class for executable "operation". Instances of it
    are specifically targeted (and possibly optimized) for an
    `array_descriptor` and `device` pair which are provided during
    construction.
    - `kernel_builder`: Abstract class to build `kernel`-s. It can be
    queried whether it supports a particular configuration. It also exposes
    an `operation_id` to easily index `kernel_builders` by the operation
    they intend to represent.
    - `kernel_manager`: Centralizes all known `kernel_builder`-s.
    
    Example implementations:
    `cpu_add_kernel` -> `kernel`
    `cpu_add_kernel_builder` -> `kernel_builder`
    `cuda_fft_kernel` -> `kernel`
    `cuda_fft_kernel_builder` -> `kernel_builder`
    [...]
    
    Why `kernel` and `kernel_builder`?
    Some operations demand some planning:
    - Elementwise operations and reduction operations involve building an
    `array_access_layout` for optimal traversal.
    - FFTs use explicit handles which are created for specific array
    layouts: `cufftHandle`, `fftw_plan`...
    
    These functions vary in complexity and cost, but they are not free. This
    separation allows to "plan once and use multiple times". Furthermore, in
    the future we would like to add kernel caching to automatically re-use
    previously built kernels.

---
+++ include/xmipp4/core/multidimensional/kernel_manager.hpp
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+
+#include "../platform/dynamic_shared_object.h"
+#include "../service_manager.hpp"
+#include "../numerical_type.hpp"
+#include "../span.hpp"
+
+#include <memory>
+
+namespace xmipp4 
+{
+namespace hardware
+{
+
+class device;
+
+} // namespace hardware
+
+namespace multidimensional
+{
+
+class operation;
+class array_descriptor;
+class kernel_builder;
+class kernel;
+
+/**
+ * @brief Centralizes all known instances of kernel builders.
+ * 
+ * The kernel builders are cataloged by their intended operation such that
+ * it allows fast retrieval.
+ */
+class kernel_manager
+	: public service_manager
+{
+public:
+	XMIPP4_CORE_API
+	kernel_manager() noexcept;
+	kernel_manager(const kernel_manager &other) = delete;
+	kernel_manager(kernel_manager &&other) = delete;
+	XMIPP4_CORE_API
+	~kernel_manager() override;
+
+	kernel_manager& operator=(const kernel_manager &other) = delete;
+	kernel_manager& operator=(kernel_manager &&other) = delete;
+
+	XMIPP4_CORE_API
+	void register_builtin_backends() override;
+
+	/**
+	 * @brief Register a new kernel.
+	 * 
+	 * @param builder The builder to be registered.
+	 * @return true The kernel builder was successfully registered.
+	 * @return false The kernel builder could not be registered.
+	 */
+	XMIPP4_CORE_API
+	bool register_kernel(std::unique_ptr<kernel_builder> builder);
+
+	/**
+	 * @brief Build an executable kernel for a given launch configuration.
+	 * 
+	 * @param operation The operation for which the kernel is built.
+	 * @param descriptors The array descriptors involved in the operation.
+	 * @param device The device where the operation is expected to be executed.
+	 * @return std::shared_ptr<kernel> The executable kernel suited for the
+	 * requested launch parameters.
+	 */
+	XMIPP4_CORE_API
+	std::shared_ptr<kernel> build_kernel(
+		const operation &operation,
+		span<const array_descriptor> descriptors,
+		hardware::device &device
+	) const;
+
+private:
+	class implementation;
+	std::unique_ptr<implementation> m_implementation;
+
+	implementation& create_if_null();
+	const implementation& get_implementation() const noexcept;
+};
+
+} // namespace multidimensional
+} // namespace xmipp4

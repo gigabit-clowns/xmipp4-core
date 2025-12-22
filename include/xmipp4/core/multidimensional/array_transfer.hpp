6083e20d436f139d35dcb8e05816aa6c1a507755
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2025-12-22 15:02:26 +0100

    Remove array cast

--- include/xmipp4/core/multidimensional/array_transfer.hpp
+++ include/xmipp4/core/multidimensional/array_transfer.hpp
@@ -13,6 +13,19 @@ class execution_context;
 namespace multidimensional
 {
 
+/**
+ * @brief Transfer an array to a different memory resource.
+ * 
+ * When the source array is already located in the requested memory resource,
+ * an alias to the original array is returned. Otherwise, a copy of the array
+ * is performed to a new array located in the requested memory resource. Note
+ * that in unified memory architectures, this function also retuns an alias.
+ * 
+ * @param source The array to be transferred.
+ * @param target_affinity Target placement for the transferred array.
+ * @param context The execution context where the transfer will be performed.
+ * @return array The transferred array.
+ */
 XMIPP4_CORE_API
 array transfer(
 	array &source, 
@@ -20,6 +33,19 @@ array transfer(
 	const execution_context &context
 );
 
+/**
+ * @brief Transfer an array to a different memory resource by copying its 
+ * contents.
+ * 
+ * Unlike `transfer`, this function always performs a copy of the source array,
+ * regardless of its current placement.
+ * 
+ * @param source The array to be transferred.
+ * @param target_affinity Target placement for the transferred array.
+ * @param context The execution context where the transfer will be performed.
+ * @param out Optional array to reuse for the result.
+ * @return array The transferred array.
+ */
 XMIPP4_CORE_API
 array transfer_copy(
 	array_view source,
@@ -27,5 +53,6 @@ array transfer_copy(
 	const execution_context &context,
 	array *out = nullptr
 );
+
 } // namespace multidimensional
 } // namespace xmipp4

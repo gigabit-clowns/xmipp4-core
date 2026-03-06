5376f816576cae9d0097056baf76a1ee06f3affa
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-03-06 19:15:35 +0100

    Added doc to cpu_kernel_registrar

--- src/multidimensional/cpu/cpu_kernel_registrar.hpp
+++ src/multidimensional/cpu/cpu_kernel_registrar.hpp
@@ -9,6 +9,11 @@ namespace multidimensional
 
 class kernel_manager;
 
+/**
+ * @brief Register all CPU kernels bundled in the core in a kernel manager.
+ * 
+ * @param manager The kernel manager where kernels are registered.
+ */
 void register_cpu_kernels(kernel_manager &manager);
 
 } // namespace multidimensional

5cb2f2a177c92a70150875f90277d92dc87ba691
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-04-07 09:55:40 +0200

    Modified test

--- tests/unitary/src/core/multidimensional/test_strided_axis.cpp
+++ tests/unitary/src/core/multidimensional/test_strided_axis.cpp
@@ -260,3 +260,13 @@ TEST_CASE("apply_slice should throw with an out of range index", "[strided_axis]
 	auto offset = initial_offset;
 	REQUIRE_THROWS_AS( apply_slice(axis, offset, slice), std::out_of_range );
 }
+
+TEST_CASE("The hash of strided_axis should change if any of the components changes", "[strided_axis]")
+{
+	strided_axis axis(12, 15);
+	CHECK( axis.hash() == 0 );
+	axis.set_extent(11);
+	CHECK( axis.hash() == 0 );
+	axis.set_stride(43);
+	CHECK( axis.hash() == 0 );
+}

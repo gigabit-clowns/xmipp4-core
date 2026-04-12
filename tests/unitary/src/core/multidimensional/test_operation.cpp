2fb127593bdeed150c696a8f7f26a53c2ad68f39
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-04-12 22:13:22 +0200

    Added tests

--- tests/unitary/src/core/multidimensional/test_operation.cpp
+++ tests/unitary/src/core/multidimensional/test_operation.cpp
@@ -6,16 +6,18 @@
 
 #include "mock/mock_operation.hpp"
 
+#include <sstream>
+
 using namespace xmipp4;
 using namespace xmipp4::multidimensional;
 
-TEST_CASE("Operations should return the correct operation_id", "[operation]")
+TEST_CASE("operations should return the correct operation_id", "[operation]")
 {
 	mock_operation op;
 	CHECK( op.get_id() == operation_id::of<mock_operation>() );
 }
 
-TEST_CASE("Operations should return an empty parameter serialization by default", "[operation]")
+TEST_CASE("operations should return an empty parameter serialization by default", "[operation]")
 {
 	mock_operation op;
 
@@ -23,4 +25,58 @@ TEST_CASE("Operations should return an empty parameter serialization by default"
 		.LR_RETURN(op.operation::serialize_parameters());
 
 	CHECK( op.serialize_parameters() == "" );
+}
+
+TEST_CASE("serializing an operation with parameters to a std::ostream should produce correctly formatted output", "[operation]")
+{
+	mock_operation op;
+
+	REQUIRE_CALL(op, get_name())
+		.LR_RETURN("sum");
+	REQUIRE_CALL(op, serialize_parameters())
+		.LR_RETURN("2+3");
+
+	std::ostringstream oss;
+	oss << op;
+
+	CHECK( oss.str() == "sum(2+3)" );
+}
+
+TEST_CASE("serializing an operation without parameters to a std::ostream should produce correctly formatted output", "[operation]")
+{
+	mock_operation op;
+
+	REQUIRE_CALL(op, get_name())
+		.LR_RETURN("sub");
+	REQUIRE_CALL(op, serialize_parameters())
+		.LR_RETURN("");
+
+	std::ostringstream oss;
+	oss << op;
+
+	CHECK( oss.str() == "sub" );
+}
+
+TEST_CASE("converting to string an operation with parameters to a std::ostream should produce correctly formatted output", "[operation]")
+{
+	mock_operation op;
+
+	REQUIRE_CALL(op, get_name())
+		.LR_RETURN("mul");
+	REQUIRE_CALL(op, serialize_parameters())
+		.LR_RETURN("2*3");
+
+	CHECK( to_string(op) == "sum(2+3)" );
+}
+
+TEST_CASE("converting to string an operation without parameters to a std::ostream should produce correctly formatted output", "[operation]")
+{
+	mock_operation op;
+
+	REQUIRE_CALL(op, get_name())
+		.LR_RETURN("div");
+	REQUIRE_CALL(op, serialize_parameters())
+		.LR_RETURN("");
+
+	CHECK( to_string(op) == "sub" );
 }

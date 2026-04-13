8a26a89da2c6c0e87fc64ec86e326984de52e65d
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-04-13 15:08:31 +0200

    Added draft implementation of trigonometric functions

---
+++ src/core/multidimensional/array_trigonometric.cpp
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include <xmipp4/core/multidimensional/array_trigonometric.hpp>
+
+#include <xmipp4/core/multidimensional/operation_execute.hpp>
+#include <xmipp4/core/multidimensional/array.hpp>
+//#include <xmipp4/core/multidimensional/operations/trigonometric_operation.hpp>
+
+#include <tuple>
+
+namespace xmipp4 
+{
+namespace multidimensional
+{
+
+array sin(
+	const array_view &x,
+	const execution_context &context,
+	array *out = nullptr
+)
+{
+	//return execute_unary(sin_operation(), x, context, out); // TODO
+}
+
+array cos(
+	const array_view &x,
+	const execution_context &context,
+	array *out = nullptr
+)
+{
+	//return execute_unary(cos_operation(), x, context, out); // TODO
+}
+
+void sincos(
+	const array_view &x,
+	const execution_context &context,
+	array &sin,
+	array &cos
+)
+{
+	std::array<array, 2> output = { sin.share(), cos.share() };
+	std::array<array_view, 1> input = { x.share() };
+
+	/*
+	execute(
+		sincos_operation(),
+		make_span(output),
+		make_span(input),
+		context
+	);
+	*/
+
+	sin = std::move(output[0]);
+	cos = std::move(output[1]);
+}
+
+array tan(
+	const array_view &x,
+	const execution_context &context,
+	array *out = nullptr
+)
+{
+	//return execute_unary(tan_operation(), x, context, out); // TODO
+}
+
+array asin(
+	const array_view &x,
+	const execution_context &context,
+	array *out = nullptr
+)
+{
+	//return execute_unary(asin_operation(), x, context, out); // TODO
+}
+
+array acos(
+	const array_view &x,
+	const execution_context &context,
+	array *out = nullptr
+)
+{
+	//return execute_unary(acos_operation(), x, context, out); // TODO
+}
+
+array atan(
+	const array_view &x,
+	const execution_context &context,
+	array *out = nullptr
+)
+{
+	//return execute_unary(atan_operation(), x, context, out); // TODO
+}
+
+array atan2(
+	const array_view &y,
+	const array_view &x,
+	const execution_context &context,
+	array *out = nullptr
+)
+{
+	//return execute_binary(atan2_operation(), y, x, context, out); // TODO
+}
+
+} // namespace multidimensional
+} // namespace xmipp4

9a18f011dff411d6fb0184ee80813617b2f5d412
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-04-13 14:31:46 +0200

    Refactored transfers

--- src/core/multidimensional/array_transfer.cpp
+++ src/core/multidimensional/array_transfer.cpp
@@ -5,6 +5,7 @@
 #include <xmipp4/core/execution_context.hpp>
 #include <xmipp4/core/multidimensional/operation_execute.hpp>
 #include <xmipp4/core/multidimensional/array_creation.hpp>
+#include <xmipp4/core/multidimensional/array_descriptor.hpp>
 #include <xmipp4/core/multidimensional/operations/copy_operation.hpp>
 #include <xmipp4/core/hardware/buffer.hpp>
 
@@ -12,6 +13,41 @@ namespace xmipp4
 {
 namespace multidimensional
 {
+
+namespace
+{
+
+array transfer_copy(
+	array_view input, 
+	hardware::memory_resource_affinity affinity,
+	const execution_context &context,
+	array *out
+)
+{
+	const auto &input_descriptor = input.get_descriptor();
+
+	std::vector<std::size_t> input_extents;
+	input_descriptor.get_layout().get_extents(input_extents);
+
+	array result = empty(
+		array_descriptor(
+			strided_layout::make_contiguous_layout(make_span(input_extents)),
+			input_descriptor.get_data_type()
+		),
+		affinity,
+		context,
+		out
+	);
+	execute(
+		copy_operation(),
+		make_span(&result, 1),
+		make_span(&input, 1),
+		context
+	);
+	return result;
+}
+
+} // anonymous namespace
 
 array to_device(array &input, const execution_context &context)
 {
@@ -39,19 +75,12 @@ array to_device_copy(
 	array *out
 )
 {
-	array result = empty(
-		input.get_descriptor(),
-		hardware::memory_resource_affinity::device,
-		context,
+	return transfer_copy(
+		std::move(input), 
+		hardware::memory_resource_affinity::device, 
+		context, 
 		out
 	);
-	execute(
-		copy_operation(),
-		make_span(&result, 1),
-		make_span(&input, 1),
-		context
-	);
-	return result;
 }
 
 array to_host(array &input, const execution_context &context)
@@ -79,19 +108,12 @@ array to_host_copy(
 	array *out
 )
 {
-	array result = empty(
-		input.get_descriptor(),
-		hardware::memory_resource_affinity::host,
-		context,
+	return transfer_copy(
+		std::move(input), 
+		hardware::memory_resource_affinity::host, 
+		context, 
 		out
 	);
-	execute(
-		copy_operation(),
-		make_span(&result, 1),
-		make_span(&input, 1),
-		context
-	);
-	return result;
 }
 
 } // namespace multidimensional

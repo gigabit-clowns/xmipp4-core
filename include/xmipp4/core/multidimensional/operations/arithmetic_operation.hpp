105764ffe4acc393f44e8bed4baf388a85349ba4
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-04-13 19:22:48 +0200

    Implemented base class for arithmetic operations

--- include/xmipp4/core/multidimensional/operations/arithmetic_operation.hpp
+++ include/xmipp4/core/multidimensional/operations/arithmetic_operation.hpp
@@ -12,12 +12,42 @@ namespace multidimensional
 {
 
 /**
- * @brief Compute `-x` for all elements.
+ * @brief Base class for unary arithmetic operations.
  * 
- * Input and output must have the same data type
+ * Mandates arithmetic data types. Input and output operands must have the same 
+ * data type.
  */
-class XMIPP4_CORE_API negate_operation
-	: public unary_elementwise_operation // TODO
+class XMIPP4_CORE_API unary_arithmetic_operation
+	: public unary_elementwise_operation
+{
+public:
+	void sanitize_operands(
+		span<array_descriptor> output_descriptors,
+		span<array_descriptor> input_descriptors
+	) const override;
+};
+
+/**
+ * @brief Base class for binary bitwise operations.
+ * 
+ * Mandates arithmetic data types. All input and output operands must have the 
+ * same data type.
+ */
+class XMIPP4_CORE_API binary_arithmetic_operation
+	: public binary_elementwise_operation
+{
+public:
+	void sanitize_operands(
+		span<array_descriptor> output_descriptors,
+		span<array_descriptor> input_descriptors
+	) const override;
+};
+
+/**
+ * @brief Compute `-x` for all elements.
+ */
+class XMIPP4_CORE_API negate_operation final
+	: public unary_arithmetic_operation
 {
 public:
 	std::string get_name() const override;
@@ -26,8 +56,8 @@ public:
 /**
  * @brief Compute `x + y` for all elements.
  */
-class XMIPP4_CORE_API add_operation
-	: public binary_elementwise_operation // TODO
+class XMIPP4_CORE_API add_operation final
+	: public binary_arithmetic_operation
 {
 public:
 	std::string get_name() const override;
@@ -36,8 +66,8 @@ public:
 /**
  * @brief Compute `x - y` for all elements.
  */
-class XMIPP4_CORE_API subtract_operation
-	: public binary_elementwise_operation // TODO
+class XMIPP4_CORE_API subtract_operation final
+	: public binary_arithmetic_operation
 {
 public:
 	std::string get_name() const override;
@@ -46,8 +76,8 @@ public:
 /**
  * @brief Compute `x * y` for all elements.
  */
-class XMIPP4_CORE_API multiply_operation
-	: public binary_elementwise_operation // TODO
+class XMIPP4_CORE_API multiply_operation final
+	: public binary_arithmetic_operation
 {
 public:
 	std::string get_name() const override;
@@ -56,8 +86,8 @@ public:
 /**
  * @brief Compute `x / y` for all elements.
  */
-class XMIPP4_CORE_API divide_operation
-	: public binary_elementwise_operation // TODO
+class XMIPP4_CORE_API divide_operation final
+	: public binary_arithmetic_operation
 {
 public:
 	std::string get_name() const override;

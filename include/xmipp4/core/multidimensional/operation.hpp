67fc6c58e09b3f0e985fa72bc8ee9d1887d06520
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-04-14 23:21:31 +0200

    Drafting new operation schema

--- include/xmipp4/core/multidimensional/operation.hpp
+++ include/xmipp4/core/multidimensional/operation.hpp
@@ -5,6 +5,7 @@
 #include "operation_id.hpp"
 
 #include <xmipp4/core/span.hpp>
+#include <xmipp4/core/numerical_type.hpp>
 #include <xmipp4/core/hardware/memory_resource_affinity.hpp>
 #include <xmipp4/core/platform/dynamic_shared_object.h>
 
@@ -18,6 +19,93 @@ namespace multidimensional
 
 class operation_id;
 class array_descriptor;
+class strided_layout;
+
+/**
+ * @brief Interface describing the validation and deduction of array shapes.
+ */
+class XMIPP4_CORE_API shape_policy
+{
+public:
+	shape_policy() noexcept;
+	shape_policy(const shape_policy &other) = delete;
+	shape_policy(shape_policy &&other) = delete;
+	virtual ~shape_policy();
+
+	shape_policy& operator=(const shape_policy &other) = delete;
+	shape_policy& operator=(shape_policy &&other) = delete;
+
+	/**
+	 * @brief Deduce the shape of the output operands. 
+	 *
+	 * The shape of the output operands is deduced from the input shape. In
+	 * addition, input operands may be broadcasted and validated.
+	 * 
+	 * @param output_layouts Layouts of output operands.
+	 * @param input_layouts Layouts of input operands.
+	 */
+	virtual void infer_output(
+		span<strided_layout> output_layouts,
+		span<strided_layout> input_layouts
+	) const = 0;
+
+	/**
+	 * @brief Validate the shape of the operands. 
+	 * 
+	 * Output operands are considered as "given" and they are not modified.
+	 * However, input operands may be broadcasted and validated to match the 
+	 * shape requirements.
+	 * 
+	 * @param output_layouts Layouts of output operands.
+	 * @param input_layouts Layouts of input operands.
+	 */
+	virtual void validate(
+		span<const strided_layout> output_layouts,
+		span<strided_layout> input_layouts
+	) const = 0;
+};
+
+/**
+ * @brief Interface describing the validation and deduction of array data types.
+ */
+class XMIPP4_CORE_API data_type_policy
+{
+public:
+	data_type_policy() noexcept;
+	data_type_policy(const data_type_policy &other) = delete;
+	data_type_policy(data_type_policy &&other) = delete;
+	virtual ~data_type_policy();
+
+	data_type_policy& operator=(const data_type_policy &other) = delete;
+	data_type_policy& operator=(data_type_policy &&other) = delete;
+
+	/**
+	 * @brief Deduce the data type of the output operands. 
+	 *
+	 * The data type of the output operands is deduced from the input shape. In
+	 * addition input types may be validated.
+	 * 
+	 * @param output_types Data types of output operands.
+	 * @param input_types Data types of input operands.
+	 */
+	virtual void infer_output(
+		span<numerical_type> output_types,
+		span<const numerical_type> input_types
+	) const = 0;
+
+	/**
+	 * @brief Validate the data types of the input and output operands. 
+	 *
+	 * Validates the data types of all operands.
+	 * 
+	 * @param output_types Data types of output operands.
+	 * @param input_types Data types of input operands.
+	 */
+	virtual void validate(
+		span<const numerical_type> output_types,
+		span<const numerical_type> input_types
+	) const = 0;
+};
 
 /**
  * @brief Abstract class that describes an operation.
@@ -25,14 +113,12 @@ class array_descriptor;
  * Subclasses of it may implement the specific behavior for each of the
  * operations.
  */
-class operation
+class XMIPP4_CORE_API operation
 {
 public:
-	XMIPP4_CORE_API
 	operation() noexcept;
 	operation(const operation &other) = delete;
 	operation(operation &&other) = delete;
-	XMIPP4_CORE_API
     virtual ~operation();
 
 	operation& operator=(const operation &other) = delete;
@@ -65,29 +151,21 @@ public:
 	 * 
 	 * @return std::string String representation of the operation parameters.
 	 */
-	XMIPP4_CORE_API
 	virtual std::string serialize_parameters() const;
 
 	/**
-	 * @brief Process and validate the input output operands
+	 * @brief Get the shape policy for the operation.
 	 * 
-	 * This method validate and adapt operands to perform the represented 
-	 * operations. Depending on the operation type and descriptors, calling
-	 * this function may:
-	 * 
-	 * - Validate invariance in the input (e.g. inner matrix dimensions in a
-	 * matrix multiplication).
-	 * - Deduce and populate output operand's descriptors when default 
-	 * constructed ones are provided.
-	 * - Perform broadcasting in input operands.
-	 * 
-	 * @param output_descriptors The output descriptors.
-	 * @param input_descriptors The input operands.
+	 * @return const shape_policy& The shape policy.
 	 */
-	virtual void sanitize_operands(
-		span<array_descriptor> output_descriptors,
-		span<array_descriptor> input_descriptors
-	) const = 0;
+	virtual const shape_policy& get_shape_policy() const noexcept = 0;
+
+	/**
+	 * @brief Get the data type policy for the operation.
+	 * 
+	 * @return const shape_policy& The data type policy.
+	 */
+	virtual const data_type_policy& get_data_type_policy() const noexcept = 0;
 };
 
 std::ostream& operator<<(std::ostream& os, const operation& op);

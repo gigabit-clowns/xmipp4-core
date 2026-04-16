2d9296d00b6482709efcb09c8b5b8c3b4adcee41
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-04-16 23:15:26 +0200

    Added complex constructors

---
+++ include/xmipp4/core/multidimensional/operations/assignment/complex_from_polar_operation.hpp
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+
+#include <xmipp4/core/multidimensional/operation.hpp>
+#include <xmipp4/core/platform/dynamic_shared_object.h>
+
+namespace xmipp4
+{
+namespace multidimensional
+{
+
+/**
+ * @brief Create a complex array from the absolute value and phase.
+ */
+class XMIPP4_CORE_API complex_from_polar_operation final
+	: public operation
+{
+public:
+	std::string get_name() const override;
+	const shape_policy& get_shape_policy() const noexcept override;
+	const data_type_policy& get_data_type_policy() const noexcept override;
+	std::size_t get_output_count() const noexcept override;
+	std::size_t get_input_count() const noexcept override;
+};
+
+} // namespace multidimensional
+} // namespace xmipp4
---
+++ src/core/multidimensional/operations/assignment/complex_from_polar_operation.cpp
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include <xmipp4/core/multidimensional/operations/assignment/complex_from_polar_operation.hpp>
+
+#include <xmipp4/core/multidimensional/shape_policies/elementwise_shape_policy.hpp>
+
+#include <xmipp4/core/multidimensional/data_type_policies/complex_from_floating_data_type_policy.hpp>
+
+namespace xmipp4
+{
+namespace multidimensional
+{
+
+std::string complex_from_polar_operation::get_name() const
+{
+	return "complex_from_polar";
+}
+
+const shape_policy& complex_from_polar_operation::get_shape_policy() const noexcept
+{
+	return elementwise_shape_policy::get();
+}
+
+const data_type_policy& complex_from_polar_operation::get_data_type_policy() const noexcept
+{
+	return complex_from_floating_data_type_policy::get();
+}
+
+std::size_t complex_from_polar_operation::get_output_count() const noexcept { return 1; }
+std::size_t complex_from_polar_operation::get_input_count() const noexcept { return 2; }
+
+} // namespace multidimensional
+} // namespace xmipp4

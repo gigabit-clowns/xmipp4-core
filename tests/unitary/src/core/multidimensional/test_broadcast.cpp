a1d83576bb0a4a0be4879376e5325a5744fab9a7
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-04-20 19:58:44 +0200

    Fix error

--- tests/unitary/src/core/multidimensional/test_broadcast.cpp
+++ tests/unitary/src/core/multidimensional/test_broadcast.cpp
@@ -8,6 +8,7 @@
 #include <catch2/matchers/catch_matchers.hpp>
 #include <catch2/matchers/catch_matchers_exception.hpp>
 
+using namespace xmipp4;
 using namespace xmipp4::multidimensional;
 
 TEST_CASE("broadcast_extents with compatible extents should succeed", "[broadcast]")
@@ -59,76 +60,76 @@ TEST_CASE("is_broadcast_compatible with equal extents should return true", "[bro
 {
 	const std::vector<std::size_t> extents1 = {5, 6, 4, 3};
 	const std::vector<std::size_t> extents2 = {5, 6, 4, 3};
-	REQUIRE(is_broadcast_compatible(extents1, extents2));
+	REQUIRE( is_broadcast_compatible(make_span(extents1), make_span(extents2)) );
 }
 
 TEST_CASE("is_broadcast_compatible with broadcast-compatible extents should return true", "[broadcast]")
 {
 	const std::vector<std::size_t> extents1 = {5, 1, 4, 3};
 	const std::vector<std::size_t> extents2 = {5, 6, 1, 3};
-	REQUIRE(is_broadcast_compatible(extents1, extents2));
+	REQUIRE( is_broadcast_compatible(make_span(extents1), make_span(extents2)) );
 }
 
 TEST_CASE("is_broadcast_compatible with different rank extents should return true when compatible", "[broadcast]")
 {
 	const std::vector<std::size_t> extents1 = {5, 1, 4, 3};
 	const std::vector<std::size_t> extents2 = {6, 1, 3};
-	REQUIRE(is_broadcast_compatible(extents1, extents2));
+	REQUIRE( is_broadcast_compatible(make_span(extents1), make_span(extents2)) );
 }
 
 TEST_CASE("is_broadcast_compatible with incompatible extents should return false", "[broadcast]")
 {
 	const std::vector<std::size_t> extents1 = {5, 1, 4, 3};
 	const std::vector<std::size_t> extents2 = {5, 6, 2, 3};
-	REQUIRE_FALSE(is_broadcast_compatible(extents1, extents2));
+	REQUIRE_FALSE( is_broadcast_compatible(make_span(extents1), make_span(extents2)) );
 }
 
 TEST_CASE("is_broadcast_compatible is symmetric", "[broadcast]")
 {
 	const std::vector<std::size_t> extents1 = {5, 1, 4, 3};
 	const std::vector<std::size_t> extents2 = {5, 6, 1, 3};
-	REQUIRE(is_broadcast_compatible(extents1, extents2) == is_broadcast_compatible(extents2, extents1));
+	REQUIRE(is_broadcast_compatible(make_span(extents1), make_span(extents2)) == is_broadcast_compatible(make_span(extents2), make_span(extents1)));
 }
 
 TEST_CASE("is_broadcastable_to with equal extents should return true", "[broadcast]")
 {
 	const std::vector<std::size_t> from_extents = {5, 6, 4, 3};
 	const std::vector<std::size_t> to_extents   = {5, 6, 4, 3};
-	REQUIRE(is_broadcastable_to(from_extents, to_extents));
+	REQUIRE(is_broadcastable_to(make_span(from_extents), make_span(to_extents)));
 }
 
 TEST_CASE("is_broadcastable_to with ones in source should return true", "[broadcast]")
 {
 	const std::vector<std::size_t> from_extents = {5, 1, 4, 1};
 	const std::vector<std::size_t> to_extents   = {5, 6, 4, 3};
-	REQUIRE(is_broadcastable_to(from_extents, to_extents));
+	REQUIRE(is_broadcastable_to(make_span(from_extents), make_span(to_extents)));
 }
 
 TEST_CASE("is_broadcastable_to with smaller rank source should return true when compatible", "[broadcast]")
 {
 	const std::vector<std::size_t> from_extents = {1, 4, 3};
 	const std::vector<std::size_t> to_extents   = {5, 6, 4, 3};
-	REQUIRE(is_broadcastable_to(from_extents, to_extents));
+	REQUIRE(is_broadcastable_to(make_span(from_extents), make_span(to_extents)));
 }
 
 TEST_CASE("is_broadcastable_to with larger rank source should return false", "[broadcast]")
 {
 	const std::vector<std::size_t> from_extents = {5, 6, 4, 3};
 	const std::vector<std::size_t> to_extents   = {6, 4, 3};
-	REQUIRE_FALSE(is_broadcastable_to(from_extents, to_extents));
+	REQUIRE_FALSE(is_broadcastable_to(make_span(from_extents), make_span(to_extents)));
 }
 
 TEST_CASE("is_broadcastable_to with mismatched non-unit extent should return false", "[broadcast]")
 {
 	const std::vector<std::size_t> from_extents = {5, 2, 4, 3};
 	const std::vector<std::size_t> to_extents   = {5, 6, 4, 3};
-	REQUIRE_FALSE(is_broadcastable_to(from_extents, to_extents));
+	REQUIRE_FALSE(is_broadcastable_to(make_span(from_extents), make_span(to_extents)));
 }
 
 TEST_CASE("is_broadcastable_to is not symmetric when source has non-unit extent matching target", "[broadcast]")
 {
 	const std::vector<std::size_t> from_extents = {1, 4, 3};
 	const std::vector<std::size_t> to_extents   = {5, 4, 3};
-	REQUIRE(is_broadcastable_to(from_extents, to_extents));
-	REQUIRE_FALSE(is_broadcastable_to(to_extents, from_extents));
+	REQUIRE(is_broadcastable_to(make_span(from_extents), make_span(to_extents)));
+	REQUIRE_FALSE(is_broadcastable_to(make_span(to_extents), make_span(from_extents)));
 }

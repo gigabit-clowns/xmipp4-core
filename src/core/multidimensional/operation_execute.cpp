2c89f578b83e900ffe9827013fbdcb90f7bd32c6
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-04-30 09:40:11 +0200

    Updated execute

--- src/core/multidimensional/operation_execute.cpp
+++ src/core/multidimensional/operation_execute.cpp
@@ -12,6 +12,7 @@
 #include <xmipp4/core/numerical_type.hpp>
 #include <xmipp4/core/multidimensional/kernel_manager.hpp>
 #include <xmipp4/core/multidimensional/operation.hpp>
+#include <xmipp4/core/multidimensional/operation_shape_policy.hpp>
 #include <xmipp4/core/hardware/memory_allocator.hpp>
 #include <xmipp4/core/hardware/device_properties.hpp>
 #include <xmipp4/core/hardware/buffer.hpp>
@@ -32,54 +33,72 @@ namespace
 {
 
 template <typename ArrayType, std::size_t N>
-boost::container::small_vector<strided_layout, N>
-extract_layouts(
+boost::container::small_vector<array_descriptor, N>
+extract_descriptors(
 	span<ArrayType> operands,
 	std::integral_constant<std::size_t, N> /*small_cap_tag*/
 )
 {
-    boost::container::small_vector<strided_layout, N> result;
-    result.reserve(operands.size());
-    for (const auto &a : operands)
-    {
-        result.push_back(a.get_descriptor().get_layout());
-    }
-    return result;
-}
-
-template <typename ArrayType, std::size_t N>
-boost::container::small_vector<numerical_type, N>
-extract_data_types(
-	span<ArrayType> operands,
-	std::integral_constant<std::size_t, N> /*small_cap_tag*/
-)
-{
-    boost::container::small_vector<numerical_type, N> result;
-    result.reserve(operands.size());
-    for (const auto &a : operands)
-    {
-        result.push_back(a.get_descriptor().get_data_type());
-    }
-    return result;
+	boost::container::small_vector<array_descriptor, N> result;
+	result.reserve(operands.size());
+	for (const auto &a : operands)
+	{
+		result.push_back(a.get_descriptor());
+	}
+	return result;
 }
 
 template <std::size_t N>
-boost::container::small_vector<array_descriptor, N>
-build_descriptors(
-	boost::container::small_vector<strided_layout, N> &&layouts,
-	const boost::container::small_vector<numerical_type, N> &data_types
+boost::container::small_vector<numerical_type, N>
+extract_data_types(
+	span<const array_descriptor> descriptors,
+	std::integral_constant<std::size_t, N> /*small_cap_tag*/
 )
 {
-	const auto n = layouts.size();
-	XMIPP4_ASSERT(n == data_types.size());
-
-	boost::container::small_vector<array_descriptor, N> result;
-	result.reserve(n);
-	for (std::size_t i = 0; i < n; ++i)
+	boost::container::small_vector<numerical_type, N> result;
+	result.reserve(descriptors.size());
+	for (const auto &d : descriptors)
 	{
-		result.emplace_back(std::move(layouts[i]), data_types[i]);
+		result.push_back(d.get_data_type());
 	}
 	return result;
+}
+
+template <std::size_t N>
+boost::container::small_vector<operation_shape_policy::shape_type, N>
+extract_shapes(
+	span<const array_descriptor> descriptors,
+	std::integral_constant<std::size_t, N> /*small_cap_tag*/
+)
+{
+	boost::container::small_vector<operation_shape_policy::shape_type, N> result;
+	result.reserve(descriptors.size());
+	for (const auto &d : descriptors)
+	{
+		operation_shape_policy::shape_type shape;
+		d.get_layout().get_extents(shape);
+		result.push_back(std::move(shape));
+	}
+	return result;
+}
+
+void update_descriptors(
+	span<array_descriptor> descriptors,
+	span<const operation_shape_policy::shape_type> shapes,
+	span<const numerical_type> data_types
+)
+{
+	const auto n = descriptors.size();
+	XMIPP4_ASSERT(n == shapes.size());
+	XMIPP4_ASSERT(n == data_types.size());
+
+	for (std::size_t i = 0; i < n; ++i)
+	{
+		descriptors[i] = array_descriptor(
+			strided_layout::make_contiguous_layout(make_span(shapes[i])),
+			data_types[i]
+		);
+	}
 }
 
 std::shared_ptr<hardware::buffer>
@@ -296,14 +315,28 @@ void execute(
 
 	validate_arity(operation, n_outputs, n_inputs);
 
-	auto output_layouts =
-		extract_layouts(output_operands, small_output_size_tag());
-	auto output_data_types =
-		extract_data_types(output_operands, small_output_size_tag());
-	auto input_layouts =
-		extract_layouts(input_operands, small_input_size_tag());
-	auto input_data_types =
-		extract_data_types(input_operands, small_input_size_tag());
+	auto output_descriptors =
+		extract_descriptors(output_operands, small_output_size_tag());
+	auto input_descriptors =
+		extract_descriptors(input_operands, small_input_size_tag());
+
+	auto output_data_types = extract_data_types(
+		make_span(output_descriptors.data(), n_outputs),
+		small_output_size_tag()
+	);
+	auto input_data_types = extract_data_types(
+		make_span(input_descriptors.data(), n_inputs),
+		small_input_size_tag()
+	);
+
+	auto output_shapes = extract_shapes(
+		make_span(output_descriptors.data(), n_outputs),
+		small_output_size_tag()
+	);
+	auto input_shapes = extract_shapes(
+		make_span(input_descriptors.data(), n_inputs),
+		small_input_size_tag()
+	);
 
 	const auto outputs_initialized = std::all_of(
 		output_operands.begin(),
@@ -317,26 +350,31 @@ void execute(
 	const auto &shape_policy = operation.get_operation_shape_policy();
 	const auto &data_type_policy = operation.get_operation_data_type_policy();
 
-	if (outputs_initialized)
+	if (!outputs_initialized)
 	{
 		shape_policy.deduce_output(
-			make_span(output_layouts.data(), n_outputs), 
-			make_span(input_layouts.data(), n_inputs)
+			make_span(output_shapes.data(), n_outputs),
+			make_span(input_shapes.data(), n_inputs)
+		);
+		data_type_policy.infer_output(
+			make_span(output_data_types.data(), n_outputs),
+			make_span(input_data_types.data(), n_inputs)
+		);
+
+		update_descriptors(
+			make_span(output_descriptors.data(), n_outputs),
+			make_span(output_shapes.data(), n_outputs),
+			make_span(output_data_types.data(), n_outputs)
 		);
 	}
 
 	shape_policy.validate(
-		make_span(output_layouts.data(), n_outputs), 
-		make_span(input_layouts.data(), n_inputs)
+		make_span(output_shapes.data(), n_outputs),
+		make_span(input_shapes.data(), n_inputs)
 	);
-
-	auto output_descriptors = build_descriptors(
-		std::move(output_layouts),// No longer needed.
-		output_data_types
-	);
-	auto input_descriptors = build_descriptors(
-		std::move(input_layouts), // No longer needed.
-		input_data_types
+	data_type_policy.validate(
+		make_span(output_data_types.data(), n_outputs),
+		make_span(input_data_types.data(), n_inputs)
 	);
 
 	auto output_storages = resolve_output_storage(

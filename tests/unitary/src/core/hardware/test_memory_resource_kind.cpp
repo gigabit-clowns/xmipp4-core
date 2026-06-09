54ee91fbe4260d6e8dc102d70a2720d5a080b96a
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-06-09 12:27:39 -0400

    Homogenizing tests

--- tests/unitary/src/core/hardware/test_memory_resource_kind.cpp
+++ tests/unitary/src/core/hardware/test_memory_resource_kind.cpp
@@ -6,17 +6,23 @@
 
 using namespace xmipp4::hardware;
 
-TEST_CASE("score_host_affinity should return the expected score for each kind")
+TEST_CASE(
+	"score_host_affinity should return the expected score for each kind",
+	"[memory_resource_kind]"
+)
 {
-    REQUIRE( score_host_affinity(memory_resource_kind::host)         == 2 );
-    REQUIRE( score_host_affinity(memory_resource_kind::host_staging) == 2 );
+    REQUIRE( score_host_affinity(memory_resource_kind::host)          == 2 );
+    REQUIRE( score_host_affinity(memory_resource_kind::host_staging)  == 2 );
     REQUIRE( score_host_affinity(memory_resource_kind::device_mapped) == 2 );
-    REQUIRE( score_host_affinity(memory_resource_kind::unified)      == 2 );
-    REQUIRE( score_host_affinity(memory_resource_kind::managed)      == 1 );
-    REQUIRE( score_host_affinity(memory_resource_kind::device_local) == 0 );
+    REQUIRE( score_host_affinity(memory_resource_kind::unified)       == 2 );
+    REQUIRE( score_host_affinity(memory_resource_kind::managed)       == 1 );
+    REQUIRE( score_host_affinity(memory_resource_kind::device_local)  == 0 );
 }
 
-TEST_CASE("score_device_affinity should return the expected score for each kind")
+TEST_CASE(
+	"score_device_affinity should return the expected score for each kind",
+	"[memory_resource_kind]"
+)
 {
     REQUIRE( score_device_affinity(memory_resource_kind::device_local)  == 4 );
     REQUIRE( score_device_affinity(memory_resource_kind::unified)       == 3 );
@@ -26,7 +32,10 @@ TEST_CASE("score_device_affinity should return the expected score for each kind"
     REQUIRE( score_device_affinity(memory_resource_kind::host)          == 0 );
 }
 
-TEST_CASE("is_host_accessible should return true iff score_host_affinity > 0")
+TEST_CASE(
+	"is_host_accessible should return true iff score_host_affinity > 0",
+	"[memory_resource_kind]"
+)
 {
     REQUIRE(  is_host_accessible(memory_resource_kind::host) );
     REQUIRE(  is_host_accessible(memory_resource_kind::host_staging) );
@@ -36,7 +45,10 @@ TEST_CASE("is_host_accessible should return true iff score_host_affinity > 0")
     REQUIRE( !is_host_accessible(memory_resource_kind::device_local) );
 }
 
-TEST_CASE("is_device_accessible should return true iff score_device_affinity > 0")
+TEST_CASE(
+	"is_device_accessible should return true iff score_device_affinity > 0",
+	"[memory_resource_kind]"
+)
 {
     REQUIRE(  is_device_accessible(memory_resource_kind::device_local) );
     REQUIRE(  is_device_accessible(memory_resource_kind::unified) );

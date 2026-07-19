c4c3876f1e1297247e41e3351c21b2a6d99be736
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-07-18 21:22:03 -0400

    Fixes in modulo

--- src/backends/cpu/builders/arithmetic/module_program_builder.cpp
+++ src/backends/cpu/builders/arithmetic/module_program_builder.cpp
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
-#include <xmipp4/ops/arithmetic/add_operation.hpp>
+#include <xmipp4/ops/arithmetic/modulo_operation.hpp>
 
 #include <backends/cpu/builders/elementwise_program_builder.hpp>
 #include <backends/cpu/builders/default_kernel_factory.hpp>
 #include <backends/cpu/builders/type_dispatchers/homogeneous_type_dispatcher.hpp>
 #include <backends/cpu/load_store.hpp>
+
+#include <cmath>
 
 namespace xmipp4
 {
@@ -15,17 +17,52 @@ namespace cpu
 namespace
 {
 
-struct add_kernel
+template <typename T>
+T to_divisor_sign(T remainder, T divisor)
+{
+	return (remainder != T(0) && (remainder < T(0)) != (divisor < T(0))) ?
+		remainder + divisor : remainder;
+}
+
+template <typename T>
+typename std::enable_if<
+	std::is_integral<T>::value && std::is_unsigned<T>::value,
+	T
+>::type
+floor_mod(T x, T y)
+{
+	return x % y;
+}
+
+template <typename T>
+typename std::enable_if<
+	std::is_integral<T>::value && std::is_signed<T>::value,
+	T
+>::type
+floor_mod(T x, T y)
+{
+	return to_divisor_sign(static_cast<T>(x % y), y);
+}
+
+template <typename T>
+typename std::enable_if<!std::is_integral<T>::value, T>::type
+floor_mod(T x, T y)
+{
+	using std::fmod;
+	return to_divisor_sign(fmod(x, y), y);
+}
+
+struct modulo_kernel
 {
 	template <typename T>
 	void operator()(T *result, const T *x, const T *y) const noexcept
 	{
-		store(result, load(x) % load(y));
+		store(result, floor_mod(load(x), load(y)));
 	}
 };
 
 template <typename T>
-struct module_predicate : std::integral_constant<
+struct modulo_predicate : std::integral_constant<
 	bool,
 	(std::is_integral<T>::value && !std::is_same<T, bool>::value) ||
 	std::is_floating_point<T>::value ||
@@ -35,10 +72,10 @@ struct module_predicate : std::integral_constant<
 } // anonymous namespace
 
 XMIPP4_REGISTER_ELEMENTWISE_PROGRAM_BUILDER(
-	add,
-	ops::add_operation,
-	default_kernel_factory<add_kernel>,
-	homogeneous_type_dispatcher<module_predicate>
+	modulo,
+	ops::modulo_operation,
+	default_kernel_factory<modulo_kernel>,
+	homogeneous_type_dispatcher<modulo_predicate>
 );
 
 } // namespace cpu
